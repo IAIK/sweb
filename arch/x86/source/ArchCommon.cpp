2eb369af134e2d5d8cd86b22bc1876f442d031fd
Philip Lawatsch <nomenquis@users.sourceforge.net>
2005-04-22 17:22:20 +0000

    forgot this little sucker

---
+++ arch/x86/source/ArchCommon.cpp
@@ -0,0 +1,30 @@
+//----------------------------------------------------------------------
+//   $Id: ArchCommon.cpp,v 1.1 2005/04/22 17:22:20 nomenquis Exp $
+//----------------------------------------------------------------------
+//
+//  $Log: $
+//----------------------------------------------------------------------
+
+
+#include "ArchCommon.h"
+
+
+uint32 ArchCommon::haveVESAConsole()
+{
+  return 0;
+}
+
+uint32 ArchCommon::getVESAConsoleHeight()
+{
+  return 0;
+}
+
+uint32 ArchCommon::getVESAConsoleWidth()
+{
+  return 0;
+}
+
+uint8 *ArchCommon::getVESAConsoleLFBPtr()
+{
+  return 0;
+}
