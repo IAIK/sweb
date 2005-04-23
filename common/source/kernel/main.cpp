484d90b0bf71ffae80e20ed9ab1711c08575f493
Bernhard Tittelbach <btittelbach@users.sourceforge.net>
2005-04-23 16:03:40 +0000

    kmm testen im main

--- common/source/kernel/main.cpp
+++ common/source/kernel/main.cpp
@@ -1,7 +1,10 @@
 /**
- * $Id: main.cpp,v 1.11 2005/04/23 15:58:32 nomenquis Exp $
+ * $Id: main.cpp,v 1.12 2005/04/23 16:03:40 btittelbach Exp $
  *
  * $Log: main.cpp,v $
+ * Revision 1.11  2005/04/23 15:58:32  nomenquis
+ * lots of new stuff
+ *
  * Revision 1.10  2005/04/23 12:52:26  nomenquis
  * fixes
  *
@@ -41,25 +44,64 @@
  *
  */
  
- 
 #include "types.h"
 #include "multiboot.h"
 #include "arch_panic.h"
 #include "paging-definitions.h"
 #include "console/ConsoleManager.h"
+#include "mm/new.h"
 #include "mm/PageManager.h"
 
 extern void* kernel_end_address;
 
 extern "C"
 {
+  
 	void startup()
 	{
-    /*
-    pointer next_usable_address = (pointer)&kernel_end_address;
-    next_usable_address = PageManager::createPageManager(next_usable_address);
-    */
-    ConsoleManager *manager = new ConsoleManager(1);
+   //KernelMemoryManager *kmm = new ((void*) &kernel_end_address) KernelMemoryManager(((pointer) &kernel_end_address )+sizeof(KernelMemoryManager),0x80400000);
+   initializeKernelMemoryManager();
+
+    char *array = new char[1024];
+    for (uint32 c=0; c<1024; ++c)
+      array[c]='X';
+    
+    char *array2 = new char[128];
+    for (uint32 c=0; c<128; ++c)
+      array2[c]='Y';
+
+    char *array3 = new char[512];
+    for (uint32 c=0; c<512; ++c)
+      array3[c]='Z';
+
+    //~ char *array2b = (char*) kmm->reallocateMemory((pointer) array2,512);
+    //~ if (array2b == array2)
+      //~ arch_panic("reallocate memory failed");
+    
+    //~ for (uint32 c=0; c<128; ++c)
+      //~ if (array2b[c]!='Y')  
+        //~ arch_panic("reallocate comparision failed");
+      
+    delete[] array;
+    delete[] array3;
+    //delete[] array2b;
+    delete[] array2;
+    
+    //~ char *x;
+    //~ x=(char*) 0x80200000;
+    //~ uint8 * fb = (uint8*)0xC00B8000;
+    //~ uint32 i=0;
+    //~ while (x < (char*)0x80400000)
+    //~ {
+      //~ fb[i++] = *x++;
+      //~ fb[i++] = 0x9f;
+    //~ }
+        
+   //arch_panic((uint8*) "und stop");
+    
+
+
+  ConsoleManager *manager = new ConsoleManager(1);
     
     Console *console = manager->getActiveConsole();
     /*uint32 i,k;
@@ -90,5 +132,5 @@ extern "C"
 
     for (;;);
 	}
-	  
+  	  
 }
