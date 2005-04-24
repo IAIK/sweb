0cb8238eb1473a6d06d7273b6ff26bb01806e264
Bernhard Tittelbach <btittelbach@users.sourceforge.net>
2005-04-24 20:31:33 +0000

    kprintf now knows how to padd

--- common/source/console/kprintf.cpp
+++ common/source/console/kprintf.cpp
@@ -1,7 +1,10 @@
 //----------------------------------------------------------------------
-//   $Id: kprintf.cpp,v 1.2 2005/04/24 18:58:45 btittelbach Exp $
+//   $Id: kprintf.cpp,v 1.3 2005/04/24 20:31:33 btittelbach Exp $
 //----------------------------------------------------------------------
 //   $Log: kprintf.cpp,v $
+//   Revision 1.2  2005/04/24 18:58:45  btittelbach
+//   kprintf bugfix
+//
 //   Revision 1.1  2005/04/24 13:33:40  btittelbach
 //   skeleton of a kprintf
 //
@@ -22,41 +25,35 @@ uint8 const LEFT	= 16;		/* left justified */
 uint8 const SPECIAL	= 32;		/* 0x */
 uint8 const LARGE	= 64;		/* use 'ABCDEF' instead of 'abcdef' */
 
-void output_number(Console *console, uint32 num, uint32 base, int32 precision, uint8 type)
+void output_number(Console *console, uint32 num, uint32 base, uint32 size, uint32 precision, uint8 type)
 {
-	//char c
-  char sign,tmp[66];
+	char c;
+  char sign,tmp[70];
 	const char *digits;
 	static const char small_digits[] = "0123456789abcdefghijklmnopqrstuvwxyz";
 	static const char large_digits[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	int32 i;
+	uint32 i;
 
 	digits = (type & LARGE) ? large_digits : small_digits;
-	//~ if (type & LEFT)
-		//~ type &= ~ZEROPAD;
+	if (type & LEFT)
+  {
+		type &= ~ZEROPAD;
+    size = 0;  //no padding then
+  }
 	if (base < 2 || base > 36)
 		return;
-	//~ c = (type & ZEROPAD) ? '0' : ' ';
+	c = (type & ZEROPAD) ? '0' : ' ';
 	sign = 0;
 	if (type & SIGN) {
 		if (((int32) num) < 0) {
 			sign = '-';
 			num = - (int32) num;
-			//size--;
 		} else if (type & PLUS) {
 			sign = '+';
-			//size--;
 		} else if (type & SPACE) {
 			sign = ' ';
-			//size--;
 		}
 	}
-	//~ if (type & SPECIAL) {
-		//~ if (base == 16)
-			//~ size -= 2;
-		//~ else if (base == 8)
-			//~ size--;
-	//~ }
 	i = 0;
 	if (num == 0)
 		tmp[i++]='0';
@@ -65,8 +62,6 @@ void output_number(Console *console, uint32 num, uint32 base, int32 precision, u
 		tmp[i++] = digits[num%base];
     num /= base;
   }
-	if (i > precision)
-		precision = i;
 	//size -= precision;
 	//~ if (!(type&(ZEROPAD+LEFT))) {
 		//~ while(size-- >0) {
@@ -74,22 +69,27 @@ void output_number(Console *console, uint32 num, uint32 base, int32 precision, u
     //~ }
 	//~ }
 	if (sign) {
-    console->write((uint8) sign);
+    tmp[i++] = sign;
   }
 	if (type & SPECIAL) {
+    precision = 0; //no precision with special for now
 		if (base==8) {
-        console->write((uint8) '0');
+        tmp[i++] = '0';
 		} else if (base==16) {
-        console->write((uint8) '0');
-        console->write((uint8) 'x');  //small x regardless of digit caps
+        tmp[i++] = digits[33];
+        tmp[i++] = '0';
       }
 	}
-	//~ if (!(type & LEFT)) {
-		//~ while (size-- > 0) {
-      //~ console->write((uint8) c); }
-	//~ }
-	while (i < precision--)         
+
+  if (precision > size)
+    precision = size;
+  
+  while (size-- - precision > i)
+    console->write((uint8) c);
+  
+  while (precision-- > i)         
     console->write((uint8) '0');
+    
 	while (i-- > 0)
     console->write((uint8) tmp[i]);
 	//~ while (size-- > 0) {
@@ -97,6 +97,20 @@ void output_number(Console *console, uint32 num, uint32 base, int32 precision, u
 			//~ *buf = ' ';
 		//~ ++buf;
 	//~ }
+}
+
+uint32 atoi(const char *&fmt)
+{
+  uint32 num=0;
+  uint32 log=0;
+  while (*fmt >= '0' && *fmt <= '9')
+  {
+    num*= (10*log);
+    num+=*fmt - '0';
+    ++log;
+    ++fmt;
+  }
+  return num;
 }
 
 // simple vkprintf, doesn't know flags yet
@@ -107,7 +121,29 @@ void vkprintf(Console *console, const char *fmt, va_list args)
   {
     if (*fmt == '%')
     {
+      int32 width = 0;
+      uint8 flag = 0;
       ++fmt;
+      switch (*fmt) 
+      {
+        case '-':
+          flag |= LEFT;
+          ++fmt;
+          break;
+        case '+':
+          flag |= PLUS;
+          ++fmt;
+          break;
+        case '0':
+          flag |= ZEROPAD;
+          ++fmt;
+          break;
+        default:
+          break;
+      }
+      if (*fmt > '0' && *fmt <= '9')
+        width = atoi(fmt);  //this advances *fmt as well
+      
       //handle diouxXfeEgGcs
       switch (*fmt)
       {
@@ -121,7 +157,7 @@ void vkprintf(Console *console, const char *fmt, va_list args)
         
         //signed decimal
         case 'd':
-          output_number(console,(uint32) va_arg(args,int32),10,0, 0 | SIGN);
+          output_number(console,(uint32) va_arg(args,int32),10,width, 0, flag | SIGN);
           break;
         
         //we don't do i until I see what it actually should do
@@ -130,20 +166,20 @@ void vkprintf(Console *console, const char *fmt, va_list args)
 
         //octal
         case 'o':
-          output_number(console,(uint32) va_arg(args,uint32),8,0, 0 | SPECIAL);
+          output_number(console,(uint32) va_arg(args,uint32),8,width, 0, flag | SPECIAL);
           break;
 
         //unsigned
         case 'u':
-          output_number(console,(uint32) va_arg(args,uint32),10,0, 0 );
+          output_number(console,(uint32) va_arg(args,uint32),10,width, 0, flag );
           break;
 
         case 'x':
-          output_number(console,(uint32) va_arg(args,uint32),16,0, 0 | SPECIAL);
+          output_number(console,(uint32) va_arg(args,uint32),16,width, 0, flag | SPECIAL);
           break;
 
         case 'X':
-          output_number(console,(uint32) va_arg(args,uint32),16,0, 0 | SPECIAL | LARGE);
+          output_number(console,(uint32) va_arg(args,uint32), 16, width, 0, flag | SPECIAL | LARGE);
           break;
         
         //no floating point yet
