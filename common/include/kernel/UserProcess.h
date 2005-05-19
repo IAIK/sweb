0b8b3f12b6e8fc41d7303c373a96212c86685a27
Bernhard Tittelbach <btittelbach@users.sourceforge.net>
2005-05-19 15:45:38 +0000

    Struktur zum Verwalten von UserProcessen, noch unfertig

---
+++ common/include/kernel/UserProcess.h
@@ -0,0 +1,56 @@
+//----------------------------------------------------------------------
+//  $Id: UserProcess.h,v 1.1 2005/05/19 15:45:38 btittelbach Exp $
+//----------------------------------------------------------------------
+//
+//  $Log: UserProcess.h,v $
+//
+//----------------------------------------------------------------------
+
+
+#ifndef _USERPROCESS_H
+#define _USERPROCESS_H
+
+#include "paging-definitions.h"
+#include "mm/PageManager.h"
+#include "../../arch/common/include/ArchMemory.h"
+#include "../../arch/arch/include/panic.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+uint32 const UserStackSize_ = 4U*1024U; //4k
+
+class UserProcess
+{
+  public:
+  UserProcess(); //will need some way to get code from somewhere (uint32 inode ??)
+  ~UserProcess();
+  
+  void installUserSpaceTable();
+  
+  private: 
+  uint32 calculateSizeNeeded(); //will need some arguments
+  void loadELF(); //will need some arguments
+  pointer allignAdress(pointer addr);
+  
+  uint32 ppn_of_pagetable_[2]; //should propably be a list instead
+  page_table_entry *page_table_;    //code and heap, 4mb limit for now...
+  page_table_entry *stack_page_table_; //stack
+  uint32 number_of_code_pages_;
+  uint32 number_of_heap_pages_;
+  uint32 number_of_stack_pages_;
+  pointer va_code_start_;  //make it simple and the code readable for now
+  pointer va_heap_start_;
+  pointer va_stack_start_;
+  //other stuff
+};
+
+#endif /* _USERPROCESS_H */
---
+++ common/source/kernel/UserProcess.cpp
@@ -0,0 +1,106 @@
+//----------------------------------------------------------------------
+//  $Id: UserProcess.cpp,v 1.1 2005/05/19 15:45:38 btittelbach Exp $
+//----------------------------------------------------------------------
+//
+//  $Log: UserProcess.cpp,v $
+//
+//----------------------------------------------------------------------
+
+#include "UserProcess.h"
+
+UserProcess::UserProcess()
+{
+  uint32 code_size = calculateSizeNeeded(); //give arguments from Constructor
+  number_of_code_pages_  = code_size / PAGE_SIZE + ((code_size % PAGE_SIZE)?1:0); 
+  number_of_heap_pages_  = 1;
+  number_of_stack_pages_ = 1;
+  va_code_start_  = 0;
+  va_heap_start_  = allignAdress(((pointer) va_code_start_ + code_size));
+  va_stack_start_ = 2U*1024U*1024U*1024U-1;  //starts at upper end of 2g and grows down
+  
+  if (number_of_code_pages_ > 1023)
+    kpanict((uint8*) "UserProcess: To many pages needed, not implemented yet\n");
+  
+  ppn_of_pagetable_[0] = PageManager::instance()->getFreePhysicalPage(); //code & heap
+  ppn_of_pagetable_[1] = PageManager::instance()->getFreePhysicalPage(); //stack
+  
+  //evil, but ok
+  page_table_ = (page_table_entry*) PageManager::instance()->get3GBAdressOfPPN(ppn_of_pagetable_[0]);
+  stack_page_table_ = (page_table_entry*) PageManager::instance()->get3GBAdressOfPPN(ppn_of_pagetable_[1]);
+  
+    
+  uint32 p=0;
+  for (p=0; p< number_of_code_pages_+1; ++p) //+1 for some initial heap space
+  {
+    uint32 ppn = PageManager::instance()->getFreePhysicalPage();  
+    //FIXXME: -> arch
+    uint32 paddr = ppn*PAGE_SIZE;
+    page_table_[p].present = 1;
+    page_table_[p].writeable = 1;
+    page_table_[p].user_access = 1;
+    page_table_[p].accessed = 0;
+    page_table_[p].dirty = 0;
+    page_table_[p].page_base_address = paddr >> 12; //only highest 20 bit of paddr matter (FIXXME -> Arch ?)
+  }
+  
+  {
+    //initial stack page:
+    uint32 ppn = PageManager::instance()->getFreePhysicalPage();
+    //FIXXME: -> arch
+    uint32 paddr = ppn*PAGE_SIZE;
+    stack_page_table_[1023].present = 1;
+    stack_page_table_[1023].writeable = 1;
+    stack_page_table_[1023].user_access = 1;
+    stack_page_table_[1023].accessed = 0;
+    stack_page_table_[1023].dirty = 0;
+    stack_page_table_[1023].page_base_address = paddr >> 12; //only highest 20 bit of paddr matter (FIXXME -> Arch ?)    
+  }
+  
+  installUserSpaceTable();
+  loadELF();
+}
+
+UserProcess::~UserProcess()
+{
+  //free code pages
+  //free pages that were allocated for heap
+  uint32 p=0;
+  for (p=0; p< number_of_code_pages_; ++p)
+    if (page_table_[p].present)
+      PageManager::instance()->freePage((page_table_[p].page_base_address << 12) / PAGE_SIZE); //belongs into arch.... FIXXME
+  
+  //free pages for stack
+  for (p=1024; p!= 0; --p) //ok page 0 wont be freed here: FIXXME
+    if (stack_page_table_[p].present)    //should be just one
+      PageManager::instance()->freePage((stack_page_table_[p].page_base_address << 12) / PAGE_SIZE); //belongs into arch.... FIXXME
+
+  //free page where pageTable is
+  PageManager::instance()->freePage(ppn_of_pagetable_[0]);
+  PageManager::instance()->freePage(ppn_of_pagetable_[1]);
+}
+  
+void UserProcess::installUserSpaceTable()
+{
+ // ArchMemory::instance()->mapPage(..,0,...);
+}
+
+uint32 UserProcess::calculateSizeNeeded()
+{
+  //FIXXME
+  return 543;
+}
+
+void UserProcess::loadELF()
+{
+  uint8 *hello_world_exe_content= (uint8*) "?syscalls?";
+  uint8 *someloop_exe_content= (uint8*) "";
+  
+  //FIXXME
+}
+
+
+pointer UserProcess::allignAdress(pointer addr)
+{
+  addr += (4 - (addr % 4)) % 4;
+  return addr;
+}
