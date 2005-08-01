5f59412c6da84ec21df928219893525ec7c61b48
Andreas Weinberger <nightcreature@users.sourceforge.net>
2005-08-01 08:20:01 +0000

    what to say...

---
+++ arch/xen/source/arch_panic.c
@@ -0,0 +1,15 @@
+//----------------------------------------------------------------------
+//   $Id: arch_panic.c,v 1.1 2005/08/01 08:20:01 nightcreature Exp $
+//----------------------------------------------------------------------
+//
+//  $Log: arch_panic.c,v $
+//
+//----------------------------------------------------------------------
+
+#include "arch_panic.h"
+
+void arch_panic(uint8 *mesg)
+{
+// FIXME ToDo: check if possible to send message to dom0
+// and suizide
+}
---
+++ arch/xen/source/assert.cpp
@@ -0,0 +1,59 @@
+//----------------------------------------------------------------------
+//   $Id: assert.cpp,v 1.1 2005/08/01 08:20:01 nightcreature Exp $
+//----------------------------------------------------------------------
+//
+//  $Log: assert.cpp,v $
+//
+//----------------------------------------------------------------------
+
+#include "assert.h"
+//#include "kprintf.h"
+//#include "panic.h"
+
+
+void pre_new_sweb_assert(uint32 condition, uint32 line, char* file)
+{
+
+    
+  char *error_string = "KERNEL PANIC: Assertion Failed in File:  on Line: ";
+  if (!condition)
+  {
+//     uint8 * fb = (uint8*)0xC00B8000;
+//     uint32 s=0;
+//     uint32 i=0;
+//     for (s=0; s<40; ++s)
+//     {
+//       fb[i++] = error_string[s];
+//       fb[i++] = 0x9f;      
+//     }
+
+//     while (file && *file)
+//     {
+//       fb[i++] = *file++;
+//       fb[i++] = 0x9f;
+//     }
+
+//     for (s=40; s<50; ++s)
+//     {
+//       fb[i++] = error_string[s];
+//       fb[i++] = 0x9f;      
+//     }
+
+//     i+=6;
+//     while (line>0)
+//     {
+//       fb[i++] = (uint8) ( 0x30 + (line%10) );
+//       fb[i] = 0x9f; 
+//       i-=2;
+//       i--;
+//       line /= 10;
+//     }
+  }
+}  
+
+
+void sweb_assert(const char *condition, uint32 line, const char* file)
+{
+//   kprintf("KERNEL PANIC: Assertaion %s in File %s on Line %d\n",condition, file, line);
+//   kpanict((uint8*) "Halting System\n");
+}
