a31b296d15843c57c7be6747224924cbafa0b38c
David Riebenbauer <davrieb@users.sourceforge.net>
2005-08-04 17:50:03 +0000

    add basic access flag definitions

--- common/include/fs/File.h
+++ common/include/fs/File.h
@@ -26,6 +26,13 @@
 class Inode;
 class Dentry;
 
+
+/// The basic access modes for files
+#define O_RDONLY    0x0000
+#define O_WRONLY    0x0001
+#define O_RDWR      0x0002
+
+
 typedef uint32 l_off_t;
 
 class File
