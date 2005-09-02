cecb4134fe632e8d055fad3f7f47ea5d87161930
Qiang Chen <qiangchen@users.sourceforge.net>
2005-09-02 15:47:09 +0000

    *** empty log message ***

--- common/include/fs/FileSystemInfo.h
+++ common/include/fs/FileSystemInfo.h
@@ -45,7 +45,17 @@ class FileSystemInfo
     { current_position_ = current_position; 
       current_position_mnt_ = current_position_mnt; }
 
-  void set_fs_alt_root(Dentry* alt_root, VfsMount* alt_root_mnt) {}
+  // void set_fs_alt_root(Dentry* alt_root, VfsMount* alt_root_mnt)
+  // { alt_root_ = alt_root; alt_root_mnt_ = alt_root_mnt; }
+  
+  Dentry* getRoot() { return root_; }
+  VfsMount* getRootMnt() { return root_mnt_; }
+  
+  Dentry* getCurrentPosition() { return current_position_; }
+  VfsMount* getCurrentPositionMnt() { return current_position_mnt_; }
+  
+  // Dentry* getAltRoot() { return alt_root_; }
+  // VfsMount* getAltRootMnt() { return alt_root_mnt_; }
 };
 
 #endif // FILESYSTEMINFO_H___
