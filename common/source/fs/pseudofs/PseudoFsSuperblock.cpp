540d6b890219e23d01fa17f728b433fc66f215dd
David Riebenbauer <davrieb@users.sourceforge.net>
2005-09-20 15:32:30 +0000

    fix loop reading creating the inodes

--- common/source/fs/pseudofs/PseudoFsSuperblock.cpp
+++ common/source/fs/pseudofs/PseudoFsSuperblock.cpp
@@ -2,8 +2,11 @@
 //
 // CVS Log Info for $RCSfile: PseudoFsSuperblock.cpp,v $
 //
-// $Id: PseudoFsSuperblock.cpp,v 1.2 2005/09/17 09:33:55 davrieb Exp $
+// $Id: PseudoFsSuperblock.cpp,v 1.3 2005/09/20 15:32:30 davrieb Exp $
 // $Log: PseudoFsSuperblock.cpp,v $
+// Revision 1.2  2005/09/17 09:33:55  davrieb
+// finished pseudofs code
+//
 // Revision 1.1  2005/09/16 16:27:40  davrieb
 // add pseudofs
 //
@@ -17,6 +20,7 @@
 #include "fs/Dentry.h"
 #include "fs/Inode.h"
 
+#include "console/kprintf.h"
 
 //----------------------------------------------------------------------
 PseudoFsSuperblock::PseudoFsSuperblock(Dentry* s_root) :
@@ -25,7 +29,7 @@ PseudoFsSuperblock::PseudoFsSuperblock(Dentry* s_root) :
   PseudoFS *pfs = PseudoFS::getInstance();
 
   uint32 num_files = pfs->getNumFiles();
-  for (uint32 fnum = 0; fnum < num_files; ++num_files)
+  for (uint32 fnum = 0; fnum < num_files; ++fnum)
   {
     char *file_name = pfs->getFileNameByNumber(fnum);
     Dentry* fdntr = new Dentry(s_root_);
@@ -44,7 +48,6 @@ Inode* PseudoFsSuperblock::createInode(Dentry* dentry, uint32 mode)
 {
   Inode *inode = (Inode*)(new PseudoFsInode(this, mode));
   int32 inode_init = inode->mknod(dentry);
-  assert(inode_init == 0);
 
   all_inodes_.pushBack(inode);
   return inode;
