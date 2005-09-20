efa5535952643be7d0e900856d6a8eeff914683b
Bernhard Tittelbach <btittelbach@users.sourceforge.net>
2005-09-20 13:28:14 +0000

    blubba

--- userspace/libc/src/start.s
+++ userspace/libc/src/start.s
@@ -1,11 +1,17 @@
 ; (c) 2005 Bernhard Tittelbach
+BITS 32
 
-extern __syscall_exit
 extern main
+extern __syscall
 
 global _start
 _start:
 	; we dont touch parameters on stack, let main deal with them (argc,argv)
 	call main
-	; return value (exit code) from main should be in eax
-	call __syscall_exit
+	push 0x00	; if we don't push stuff onto the stack prior to calling __syscall
+	push 0x00	; __syscall will read beyond the start of the stack
+	push 0x00	; and trigger a pagefault
+	push 0x00
+	push eax		; return value (exit code) from main should be in eax
+	push 0x01	; syscall code for exit == 1
+	call __syscall
