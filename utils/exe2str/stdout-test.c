53d0267e5089d620c1b5a76084243c3fb5de66f5
Bernhard Tittelbach <btittelbach@users.sourceforge.net>
2005-09-21 20:06:41 +0000

    cleanup

--- utils/exe2str/stdout-test.c
+++
@@ -1,9 +0,0 @@
-#include "../../userspace/start.c"
-
-int main(int argc, char *argv[]) 
-{
-
-	print_to_stdout("Hi, ich bin ein Userspace Programm\n");
-	print_to_stdout("und ich schreibe einen String auf die Console\n");
-	print_to_stdout("bye bye\n");
-}
