3b8319257379a7fb7efbff1943386007f9c14bad
Bernhard Tittelbach <btittelbach@users.sourceforge.net>
2007-05-27 14:07:12 +0200

    Kommentar weg

--- userspace/tests/stdout-test.c
+++ userspace/tests/stdout-test.c
@@ -1,16 +1,6 @@
 #include "unistd.h"
 #include "stdio.h"
 
-//~ void print_to_stdout(char *string)
-//~ {
-  //~ int strlen = 0;
-  //~ char *string2=string;
-  //~ while (*string2++)
-    //~ strlen++;
-  
-  //~ __syscall(sc_write,fd_stdout,(long) string,strlen);
-//~ }
-
 int main(int argc, char *argv[]) 
 {
 	char* str1="Hi, ich bin ein Userspace Programm\n";
