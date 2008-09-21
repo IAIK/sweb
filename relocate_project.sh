d1b0755c2bfbc46fe796affa449c2fb4f68e8316
Andreas Reiter <andreas.reiter@student.tugraz.at>
2008-09-21 14:14:00 +0200

    Added GDB Macros for PageFault backtrace

--- relocate_project.sh
+++ relocate_project.sh
@@ -1,7 +1,6 @@
-echo -e "\n\n\E[31mRemoving CMake specific files and reconfiguring CMake...\n"
+printf "\n\n\E[31mRemoving CMake specific files...\n"
 tput sgr0
+printf "\nTo regenerate your makefiles run 'cmake .'\n\n"
 find -name 'CMakeCache.txt' -exec rm {} +;
 find -name 'CMakeFiles' -exec rm -f -r {} +;
 find -name 'cmake_install.cmake' -exec rm -f -r {} +;
-rm -r bin/*
-cmake .
