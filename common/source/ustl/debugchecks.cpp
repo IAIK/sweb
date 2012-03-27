4e621b878175278c81960d6fa3190aa047be1b06
Daniel Gruss <gruss@student.tugraz.at>
2012-03-27 23:02:00 +0200

    improved debug check

--- common/source/ustl/debugchecks.cpp
+++ common/source/ustl/debugchecks.cpp
@@ -1,11 +1,23 @@
 #include "ArchInterrupts.h"
 #include "Scheduler.h"
+#include "kprintf.h"
 #include "KernelMemoryManager.h"
+#include "Thread.h"
+#include "types.h"
 
-namespace ustl {
+namespace ustl
+{
   void checkKMMDeadlock()
   {
-    if (ArchInterrupts::testIFSet() == false || Scheduler::instance()->isSchedulingEnabled() == false)
-        assert(KernelMemoryManager::instance()->KMMLockHeldBy() == 0);
+    if (unlikely (ArchInterrupts::testIFSet() == false || Scheduler::instance()->isSchedulingEnabled() == false))
+    {
+      if (unlikely (KernelMemoryManager::instance()->KMMLockHeldBy() != 0))
+      {
+        kprintfd("(ERROR) checkKMMDeadlock: Using a not resize-safe ustl container method with IF=%d and SchedulingEnabled=%d ! This will fail!!!\n",
+                 ArchInterrupts::testIFSet(), Scheduler::instance()->isSchedulingEnabled());
+        currentThread->printBacktrace(true);
+        prenew_assert(false);
+      }
+    }
   }
 }
