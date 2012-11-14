7c39714ebfe20f76d661d4f5a64cf19cc4486ab5
Daniel Gruss <gruss@student.tugraz.at>
2012-11-14 17:35:11 +0100

    fixed bug in mutex

--- common/source/kernel/Mutex.cpp
+++ common/source/kernel/Mutex.cpp
@@ -83,8 +83,8 @@ void Mutex::release(const char* debug_info)
 {
   checkInvalidRelease("Mutex::release", debug_info);
   //kprintfd_nosleep("Mutex::release %x %s, %s\n", this, name_, debug_info);
-  mutex_ = 0;
   held_by_=0;
+  mutex_ = 0;
   spinlock_.acquire();
   if ( ! sleepers_.empty() )
   {
