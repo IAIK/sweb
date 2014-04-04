4b51905c2e945d77f80f8f0abe2c5d8f4a26de26
Daniel Gruss <gruss@student.tugraz.at>
2014-04-04 18:02:00 +0200

    put user_progs in an own header file

---
+++ common/include/kernel/user_progs.h
@@ -0,0 +1,14 @@
+/**
+ * @file user_progs.h
+ */
+#ifndef _USER_PROGS_H_
+#define _USER_PROGS_H_
+
+// DO NOT CHANGE THE NAME OR THE TYPE OF THE user_progs VARIABLE!
+char const *user_progs[] = {
+// for reasons of automated testing
+                            "/stdin-test.sweb",
+                            0
+                           };
+
+#endif
