873f6dd8c8bb2364f6554c0f708baea0cea00c4e
Alexander Skiba <mail@ghostlyrics.net>
2014-09-05 16:38:22 +0200

    improve prompting for "mrproper"
    - allow executing the script
    - add hint to output what the default setting does
    - catch empty strings and default to YES
    - document the script
    - good variable names

--- utils/prompt.sh
+++ utils/prompt.sh
@@ -1,6 +1,17 @@
-#!/bin/bash
-read -p "$1" yn
-case $yn in
+#!/usr/bin/env bash
+
+# prompt users when removing whole directories
+# used when making target "mrproper"
+
+read -p "$1" answer_from_user
+
+# early exit for lazy users
+if [[ -z $answer_from_user ]]; then
+  exit 0
+fi
+
+# proper case handling
+case $answer_from_user in
 	[Yy]* ) exit 0;;
 	* ) exit -1;;
 esac
