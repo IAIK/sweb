9b90d0951558d98343e5bc7a3969beb60dd6c9df
Philip Lawatsch <philip@lawatsch.at>
2014-11-13 21:09:15 +0100

    add stack unwinding for userspace threads

--- arch/arm/common/source/arch_backtrace.cpp
+++ arch/arm/common/source/arch_backtrace.cpp
@@ -52,3 +52,35 @@ int backtrace(pointer *call_stack, int size, Thread *thread, bool use_stored_reg
 
   return i;
 }
+
+int backtrace_user(pointer *call_stack, int size, Thread *thread, bool /*use_stored_registers*/)
+{
+  if (!call_stack || !size || thread != currentThread || !thread->user_arch_thread_info_)
+    return 0;
+
+  void *ebp = (void*)thread->user_arch_thread_info_->r11;
+  StackFrame *CurrentFrame = (StackFrame*)ebp;
+
+  // the userspace stack is allowed to be anywhere in userspace
+  void *StackStart = (void*) (2U*1024U*1024U*1024U - sizeof ( pointer )); // the stack "starts" at the high addresses...
+  void *StackEnd = 0x0; // ... and "ends" at the lower ones.
+
+  void *StartAddress = (void*)0x00000000;
+  void *EndAddress = (void*)  0x80000000;
+
+  int i = 0;
+  while (i < size &&
+      ADDRESS_BETWEEN(CurrentFrame, StackEnd, StackStart))
+  {
+    call_stack[i++] = (pointer)CurrentFrame->pc;
+    CurrentFrame = CurrentFrame->prev_fp;
+  }
+
+  return i;
+}
+extern "C"
+{
+void fprintf(){}
+}
+
+
