0d1249abb5903b3d5b498109a8b9c7ef93969f30
Daniel Gruss <gruss@student.tugraz.at>
2014-12-27 00:53:17 +0100

    replaced boot.s by boot.cpp - sweb on arm has now not a single .s file in use anymore... booting starts directly in C/C++

---
+++ arch/arm/common/source/boot.cpp
@@ -0,0 +1,46 @@
+#include "types.h"
+#include "board_constants.h"
+#include "init_boottime_pagetables.h"
+#include "assert.h"
+
+extern "C" void startup();
+extern "C" void __attribute__((naked)) PagingMode();
+
+uint8 interrupt_stack[0x4000] __attribute__((aligned(0x4000)));
+page_directory_entry kernel_page_directory_start[0x1000] __attribute__((aligned(0x4000))); // space for page directory
+
+extern "C" void __attribute__((naked)) entry()
+{
+  asm("mov fp, #0\n"
+      "mov sp, %[v]" : : [v]"r"(interrupt_stack + (BOARD_LOAD_BASE - 0x80000000) + 0x4000)); // Set up the stack
+  void (*initialiseBootTimePagingPTR)() = (void(*)())((uint8*)&initialiseBootTimePaging + (BOARD_LOAD_BASE - 0x80000000));
+  initialiseBootTimePagingPTR();
+  asm("mcr p15, 0, %[v], c2, c0, 0\n" : : [v]"r"(((uint8*)kernel_page_directory_start) + (BOARD_LOAD_BASE - 0x80000000))); // set ttbr0
+  asm("mcr p15, 0, %[v], c8, c7, 0\n" : : [v]"r"(0)); // tlb flush
+  asm("mcr p15, 0, %[v], c3, c0, 0\n" : : [v]"r"(3)); // set domain access control (full address space access for userspace)
+  asm("mrc p15, 0, r0, c1, c0, 0\n"
+      "orr r0, r0, #0x1\n"          // set paging bit
+      "bic r0, r0, #0x2\n"          // disable alignment fault bit
+      "orr r0, r0, #0x400000\n"     // set unaligned memory access bit
+      "mcr p15, 0, r0, c1, c0, 0\n" // enable paging
+     );
+  void (*PagingModePTR)() = &PagingMode; // create a blx jump instead of a bl jump
+  PagingModePTR();
+  assert(false && "it should be impossible to get to this point");
+}
+
+extern "C" void __attribute__((naked)) PagingMode()
+{
+  asm("mrs r0, cpsr\n"
+      "bic r0, r0, #0xdf\n"
+      "orr r0, r0, #0xdf\n"
+      "msr cpsr, r0\n"
+      "mov sp, %[v]\n" // Set up the stack
+      :
+      : [v]"r"(interrupt_stack+4096)
+      );
+  removeBootTimeIdentMapping();
+  void (*startupPTR)() = &startup; // create a blx jump instead of a bl jump
+  startupPTR();
+  assert(false && "you should never get to this point");
+}
