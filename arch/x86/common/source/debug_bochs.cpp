54fb660c626da869bcae0dfae5491adc561e8514
Daniel Gruss <gruss@student.tugraz.at>
2015-01-16 09:13:20 +0100

    removed [SWEB] tag from debug_bochs

--- arch/x86/common/source/debug_bochs.cpp
+++ arch/x86/common/source/debug_bochs.cpp
@@ -14,16 +14,11 @@ void writeChar2Bochs( char char2Write )
 
 void writeLine2Bochs( const char * line2Write )
 {
-  // TODO: It would be nice to have a sprintf function
-  // so we can format the string
-  const char *currentChar;
-  const char *swebTag = ( char * ) "[SWEB] ";
-
-  for( currentChar = swebTag; (*currentChar != '\0'); currentChar++ )
-    writeChar2Bochs( *currentChar );
-
   uint8 counter = 0; // the message is cut off at 250 chars 
 
-  for( currentChar = line2Write; (*currentChar != '\0') && (counter++ < 250); currentChar++ )
-    writeChar2Bochs( *currentChar );
+  while (*line2Write && counter++ < 250)
+  {
+    writeChar2Bochs( *line2Write );
+    ++line2Write;
+  }
 }
