e3fa47a060bfb093931743246d7b2f0bdc3c73d7
Daniel Gruss <gruss@student.tugraz.at>
2015-01-26 00:26:10 +0100

    removed unused functions in exe2minixfs VfsSyscall

--- utils/exe2minixfs/VfsSyscall.h
+++ utils/exe2minixfs/VfsSyscall.h
@@ -30,38 +30,6 @@ class VfsSyscall
   public:
 
     /**
-     * make a new directory.
-     * i.e. im the path "/file/test/" create a new directory with the name
-     * "dir". => the new_dir ist "/file/test/dir"
-     * @param pathname the new directory.
-     * @param type the permission.
-     * @return On success, zero is returned. On error, -1 is returned.
-     */
-    static int32 mkdir ( const char* pathname, int32 /*type*/ );
-
-    /**
-     * The readdir() display the names from all childs and returns a pointer
-     * to a Dirent.
-     * @param pathname the destination-directory.
-     * @return the dirent
-     */
-    static Dirent* readdir ( const char* pathname );
-
-    /**
-     * chdir() changes the current directory to the specified directory.
-     * @param dir the destination-directory.
-     * @return On success, zero is returned. On error, -1 is returned.
-     */
-    static int32 chdir ( const char* pathname );
-
-    /**
-     * delete a directory, which must be empty.
-     * @param pathname the removed directory
-     * @return On success, zero is returned. On error, -1 is returned.
-     */
-    static int32 rmdir ( const char* pathname );
-
-    /**
      * remove a directory (which must be empty) or a file
      * @param pathname the removed directory or file
      * @return On success, zero is returned. On error, -1 is returned.
@@ -85,18 +53,6 @@ class VfsSyscall
     static int32 close ( uint32 fd );
 
     /**
-     * The read() attempts to read up to count bytes from file descriptor fd
-     * into the buffer starting at buffter.
-     * @param fd the file descriptor
-     * @param buffer the buffer that to read the date
-     * @param count the size of the byte
-     * @return On success, the number of bytes read is returned (zero indicates
-     *         end of file), and the file position is advanced by this number.
-     *         On error, -1 is returned.
-     */
-    static int32 read ( uint32 fd, char* buffer, uint32 count );
-
-    /**
      * write  writes  up  to  count  bytes  to the file referenced by the file
      * descriptor fd from the buffer starting at buf.
      * @param fd the file descriptor
@@ -114,13 +70,6 @@ class VfsSyscall
      * @return 0 on success, -1 on error
      */
     static int32 flush ( uint32 fd );
-
-    /**
-     * returns the size of a file
-     * @param fd the file looking for
-     * @return the size
-     */
-    static uint32 getFileSize ( uint32 fd );
 
     /**
      * get the File descriptor object from the global variable
