7c0102c705ed82b2b1af57d09ea9f4052ed66185
Daniel Gruss <gruss@student.tugraz.at>
2015-01-31 12:35:58 +0100

    rename stdin-test to shell and remove the useless printf test

--- userspace/tests/shell.c
+++ userspace/tests/shell.c
@@ -3,6 +3,7 @@
 #include "string.h"
 #include "stdlib.h"
 #include "nonstd.h"
+#include "sys/syscall.h"
 
 #define FORK_ENABLED 0
 #define EXECUTABLE_PREFIX       "/usr/"
@@ -50,36 +51,10 @@ void handle_command(char* buffer,int buffer_size)
     }
   }
   if (strcmp(command, "ls") == 0)
-    printf("Sorry, Filesystem Syscalls not implemented yet\n");
-  else if (strcmp(command, "ps") == 0)
-    printf("Sorry, Threading System Syscalls not Implemented,\nuse F12 to print a list of Threads to Bochs\n");
+    __syscall(sc_pseudols,(size_t)args[0],0,0,0,0);
   else if (buffer[0]=='h' && buffer[1] == 'e' && buffer[2] == 'l' && buffer[3] == 'p')
   {
-    printf("Command Help:\nhelp                  yes, here we are\nexit [exit_code]      is really the only command that does something right now\nls                    does nothing\nps                    does nothing as well\ntest <num> [&& cmd]   evaluates integer x==0:false, x<>0:true, default: false\n\n");
-  }
-  else if (strcmp(command, "test") == 0)
-  {
-    c=4;
-    while (buffer[c] ==' ')
-      c++;
-    num=atoi(&buffer[c]);
-    if (num==0)
-      printf("Result: false\n"); 
-    else
-    {
-      printf("Result: true\n"); 
-      while (c < buffer_size)
-        if (buffer[c] =='&' && buffer[c+1]=='&')
-        {
-          c+=2;
-          while (buffer[c] ==' ')
-            c++;
-          handle_command(&buffer[c],buffer_size-c);
-          break;
-        }
-        else
-          c++;
-    }
+    printf("Command Help:\nhelp                  yes, here we are\nexit [exit_code]      is really the only command that does something right now\nls                    pseudo ls\n\n");
   }
   else if (strcmp(command, "exit") == 0)
   {
@@ -111,8 +86,13 @@ void handle_command(char* buffer,int buffer_size)
   }
   else
   {
-    memcpy(executable, EXECUTABLE_PREFIX, EXECUTABLE_PREFIX_LEN);
-    memcpy(executable + EXECUTABLE_PREFIX_LEN, command, strlen(command) + 1);
+    if (command[0] != '/')
+    {
+      memcpy(executable, EXECUTABLE_PREFIX, EXECUTABLE_PREFIX_LEN);
+      memcpy(executable + EXECUTABLE_PREFIX_LEN, command, strlen(command) + 1);
+    }
+    else
+      memcpy(executable, command, strlen(command) + 1);
     pid = createprocess(executable, 1);
     if (pid == -1)
     {
