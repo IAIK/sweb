ccefc11851ae45d02008324168b2720438b915bb
Daniel Gruss <gruss@student.tugraz.at>
2015-02-08 14:15:34 +0100

    reformatting

--- arch/common/include/ArchCommon.h
+++ arch/common/include/ArchCommon.h
@@ -8,89 +8,89 @@ class Console;
 
 class ArchCommon
 {
-public:
-  static pointer getKernelEndAddress();
-  static pointer getFreeKernelMemoryStart();
-  static pointer getFreeKernelMemoryEnd();
+  public:
+    static pointer getKernelEndAddress();
+    static pointer getFreeKernelMemoryStart();
+    static pointer getFreeKernelMemoryEnd();
 
-  /** 
-   * @param is_paging_set_up inform function that paging is not yet up (=false) and
-   * that it should behave accordingly
-   */
-  static uint32 haveVESAConsole(uint32 is_paging_set_up = 1);
-  static uint32 getVESAConsoleHeight();
-  static uint32 getVESAConsoleWidth();
-  static uint32 getVESAConsoleBitsPerPixel();
+    /**
+     * @param is_paging_set_up inform function that paging is not yet up (=false) and
+     * that it should behave accordingly
+     */
+    static uint32 haveVESAConsole(uint32 is_paging_set_up = 1);
+    static uint32 getVESAConsoleHeight();
+    static uint32 getVESAConsoleWidth();
+    static uint32 getVESAConsoleBitsPerPixel();
 
-  /** 
-   * @return a Pointer to the location of the VESA Memory Region
-   */
-  static pointer getVESAConsoleLFBPtr(uint32 is_paging_set_up = 1);
+    /**
+     * @return a Pointer to the location of the VESA Memory Region
+     */
+    static pointer getVESAConsoleLFBPtr(uint32 is_paging_set_up = 1);
 
-  /**
-   * @return a Pointer to the location of the FrameBuffer
-   */
-  static pointer getFBPtr(uint32 is_paging_set_up = 1);
+    /**
+     * @return a Pointer to the location of the FrameBuffer
+     */
+    static pointer getFBPtr(uint32 is_paging_set_up = 1);
 
-  /** 
-   * @return number of Useable Memory Regions
-   */
-  static uint32 getNumUseableMemoryRegions();
+    /**
+     * @return number of Useable Memory Regions
+     */
+    static uint32 getNumUseableMemoryRegions();
 
-  /**
-   * Reads the Grub MultiBoot-Info and
-   * returns the start- and end-address of the Useable Memory Region x
-   *
-   * @param number of the region-information to parse
-   * @param &start_address of Useable Memory Region
-   * @param &end_address of Useable Memory Region
-   * @param &type of Useable Memory Region
-   * @return 1 if region >= number_of_regions, 0 otherwise
-   */
-  static uint32 getUsableMemoryRegion(size_t region, pointer &start_address, pointer &end_address, size_t &type);
+    /**
+     * Reads the Grub MultiBoot-Info and
+     * returns the start- and end-address of the Useable Memory Region x
+     *
+     * @param number of the region-information to parse
+     * @param &start_address of Useable Memory Region
+     * @param &end_address of Useable Memory Region
+     * @param &type of Useable Memory Region
+     * @return 1 if region >= number_of_regions, 0 otherwise
+     */
+    static uint32 getUsableMemoryRegion(size_t region, pointer &start_address, pointer &end_address, size_t &type);
 
-  /** 
-   * @return uint32 returns the number of modules loaded by grub
-   */
-  static uint32 getNumModules(uint32 is_paging_set_up = 1);
+    /**
+     * @return uint32 returns the number of modules loaded by grub
+     */
+    static uint32 getNumModules(uint32 is_paging_set_up = 1);
 
-  /**
-   * Parses the Grub MultiBoot Info with regard to modules
-   *
-   * @param num the number of grub-loaded module which this is about
-   * @return uint32 returns memory start address of module "num"
-   */
-  static uint32 getModuleStartAddress(uint32 num, uint32 is_paging_set_up = 1);
+    /**
+     * Parses the Grub MultiBoot Info with regard to modules
+     *
+     * @param num the number of grub-loaded module which this is about
+     * @return uint32 returns memory start address of module "num"
+     */
+    static uint32 getModuleStartAddress(uint32 num, uint32 is_paging_set_up = 1);
 
-  /**
-   * Parses the Grub MultiBoot Info with regard to modules
-   *
-   * @param num the number of grub-loaded module which this is about
-   * @return uint32 returns memory end address of module "num"
-   */
-  static uint32 getModuleEndAddress(uint32 num, uint32 is_paging_set_up = 1);
+    /**
+     * Parses the Grub MultiBoot Info with regard to modules
+     *
+     * @param num the number of grub-loaded module which this is about
+     * @return uint32 returns memory end address of module "num"
+     */
+    static uint32 getModuleEndAddress(uint32 num, uint32 is_paging_set_up = 1);
 
-  /**
-   * Generates the according console depending on the architecture
-   * @param count the number of consoles to create
-   * @return a pointer to the Console object
-   */
-  static Console* createConsole(uint32 count);
+    /**
+     * Generates the according console depending on the architecture
+     * @param count the number of consoles to create
+     * @return a pointer to the Console object
+     */
+    static Console* createConsole(uint32 count);
 
-  /**
-   * Generates the Debug info depending on the architecture
-   */
-  static void initDebug();
+    /**
+     * Generates the Debug info depending on the architecture
+     */
+    static void initDebug();
 
-  /**
-   * let the cpu idle, f.e. wih the halt statement
-   */
-  static void idle();
+    /**
+     * let the cpu idle, f.e. wih the halt statement
+     */
+    static void idle();
 
-  /**
-   * draw a heartbeat character
-   */
-  static void drawHeartBeat();
+    /**
+     * draw a heartbeat character
+     */
+    static void drawHeartBeat();
 };
 
 #endif
