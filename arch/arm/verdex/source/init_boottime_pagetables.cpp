ef8d8307a774c62dc85b2fa83a25f7fc480eb8f3
Martin Skargeth <martin.skargeth@student.tugraz.at>
2015-02-21 21:47:04 +0100

    fix init_bootime_pagetabels for verdex (nor working)

--- arch/arm/verdex/source/init_boottime_pagetables.cpp
+++ arch/arm/verdex/source/init_boottime_pagetables.cpp
@@ -10,23 +10,50 @@
 
 extern "C" void initialiseBootTimePaging()
 {
-  PageDirEntry *pde_start = (PageDirEntry*)(((char*)kernel_page_directory) - PHYSICAL_TO_VIRTUAL_OFFSET);
+  PageDirEntry *pde_start = (PageDirEntry*) VIRTUAL_TO_PHYSICAL_BOOT((pointer )kernel_page_directory);
+  PageTableEntry *pte_start = (PageTableEntry*) VIRTUAL_TO_PHYSICAL_BOOT((pointer )kernel_page_tables);
 
   uint32 i;
   // the verdex board has physical ram mapped to 0xA0000000
   uint32 base = 0xA00;
+  uint32 base_4k = 0xA0000;
   // clear the page dir
   for (i = 0; i < 4096; ++i)
     *((uint32*)pde_start) = 0;
   // 1 : 1 mapping of the first 8 mbs
+  // map 16 mb PTs for kernel
+  for (i = 0; i < 16; ++i)
+  {
+    pde_start[2048 + i].pt.size = 1;
+    pde_start[2048 + i].pt.offset = i % 4;
+    pde_start[2048 + i].pt.pt_ppn = (((pointer) &pte_start[PAGE_TABLE_ENTRIES * i]) / PAGE_SIZE) + base_4k;
+  }
+  // clear the page tables
+  for (i = 0; i < 16 * PAGE_TABLE_ENTRIES; ++i)
+    *((uint32*)pte_start + i) = 0;
+  // map kernel into PTs
+  size_t kernel_last_page = (size_t)VIRTUAL_TO_PHYSICAL_BOOT((pointer)&kernel_end_address) / PAGE_SIZE;
+  extern size_t ro_data_end_address;
+  size_t last_ro_data_page = (size_t)VIRTUAL_TO_PHYSICAL_BOOT((pointer)&ro_data_end_address) / PAGE_SIZE;
+
+  for (i = 0; i < last_ro_data_page; ++i)
+  {
+    pte_start[i].size = 2;
+    pte_start[i].permissions = 1;
+    pte_start[i].page_ppn = i + base_4k;
+  }
+  for (; i < kernel_last_page; ++i)
+  {
+    pte_start[i].size = 2;
+    pte_start[i].permissions = 1;
+    pte_start[i].page_ppn = i + base_4k;
+  }
+
   for (i = 0; i < 8; ++i)
     mapBootTimePage(pde_start, i, base + i);
   // 1 : 1 mapping of the first 8 mbs of physical ram
   for (i = 0; i < 8; ++i)
     mapBootTimePage(pde_start, base + i, base + i);
-  // map first 4 mb for kernel
-  for (i = 0; i < 4; ++i)
-    mapBootTimePage(pde_start, 0x800 + i, base + i);
   // 3gb 1:1 mapping
   for (i = 0; i < 1024; ++i)
     mapBootTimePage(pde_start, 0xC00 + i, base + i);
