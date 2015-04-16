2eba229ca3dd65a1d84688cb484b2d29d66edbf5
Daniel Gruss <gruss@student.tugraz.at>
2015-04-16 14:54:52 +0200

    add assertion in CleanupThread::kill()

--- common/include/kernel/CleanupThread.h
+++ common/include/kernel/CleanupThread.h
@@ -8,6 +8,7 @@ class CleanupThread : public Thread
   public:
     CleanupThread();
     virtual ~CleanupThread();
+    virtual void kill();
     virtual void Run();
 };
 
--- common/source/kernel/CleanupThread.cpp
+++ common/source/kernel/CleanupThread.cpp
@@ -8,7 +8,12 @@ CleanupThread::CleanupThread() : Thread(0, "CleanupThread")
 
 CleanupThread::~CleanupThread()
 {
-  assert(false && "CleanupThread destruction means that you have accessed an invalid pointer somewhere.");
+  assert(false && "CleanupThread destruction means that you probably have accessed an invalid pointer somewhere.");
+}
+
+void CleanupThread::kill()
+{
+  assert(false && "CleanupThread destruction means that you probably have accessed an invalid pointer somewhere.");
 }
 
 void CleanupThread::Run()
