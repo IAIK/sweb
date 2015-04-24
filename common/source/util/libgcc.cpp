2ee94a69b612e51b802d98d4cc9e60d3727b6562
Daniel Gruss <gruss@student.tugraz.at>
2015-04-24 12:01:16 +0200

    more self explaining assertion

--- common/source/util/libgcc.cpp
+++ common/source/util/libgcc.cpp
@@ -41,10 +41,7 @@ uint64 __umoddi3(uint64 a, uint64 b)
 uint64 __udivmoddi4(uint64 num, uint64 den, uint64 *rem_p)
 {
   uint64 quot = 0, qbit = 1;
-  if ( den == 0 )
-  {
-    assert(0);
-  }
+  assert(den && "division by zero");
 
   while ( (int64)den >= 0 )
   {
