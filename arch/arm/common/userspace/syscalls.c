1420ce8fcd0ff098672ac1b334ff364a2273d877
Michael Schwarz <michael.schwarz@student.tugraz.at>
2015-12-03 15:37:13 +0100

    fixed syscall parameter 4 and 5 for arm

--- arch/arm/common/userspace/syscalls.c
+++ arch/arm/common/userspace/syscalls.c
@@ -1,14 +1,18 @@
 #include "stdlib.h"
 
-void __attribute__((naked)) __syscall()
-{
-  asm("push {r4,r5}\n"
-      "ldr r4, [fp, #-12]\n"
-      "ldr r5, [fp, #-8]\n"
-      "svc 0\n"
-      "pop {r4,r5}\n"
-      "bx lr");
+size_t __syscall(size_t arg1, size_t arg2, size_t arg3, size_t arg4,
+		size_t arg5, size_t arg6) {
+
+	register long r0 asm ("r0") = arg1;
+	register long r1 asm ("r1") = arg2;
+	register long r2 asm ("r2") = arg3;
+	register long r3 asm ("r3") = arg4;
+	register long r4 asm ("r4") = arg5;
+	register long r5 asm ("r5") = arg6;
+	asm("svc 0\n" : "=r"(r0) : "r"(r0), "r"(r1), "r"(r2), "r"(r3), "r"(r4), "r"(r5));
+	return arg1;
 }
+
 
 void abort()
 {
