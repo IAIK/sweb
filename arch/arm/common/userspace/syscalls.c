4eb42a354a77aa3546772092a939e3ddc45c015c
Thomas Bohnstingl <bohnstingl@student.tugraz.at>
2016-01-10 18:35:35 +0100

    *) Fixed returcode issue

--- arch/arm/common/userspace/syscalls.c
+++ arch/arm/common/userspace/syscalls.c
@@ -9,7 +9,10 @@ size_t __syscall(size_t arg1, size_t arg2, size_t arg3, size_t arg4,
 	register long r3 asm ("r3") = arg4;
 	register long r4 asm ("r4") = arg5;
 	register long r5 asm ("r5") = arg6;
-	asm("svc 0\n" : "=r"(r0) : "r"(r0), "r"(r1), "r"(r2), "r"(r3), "r"(r4), "r"(r5));
+	__asm__ volatile("svc 0\n\t"
+	                 "mov %[re], %%r0\n\t"
+	                 : [re] "=&r" (arg1)
+	                 : "r"(r0), "r"(r1), "r"(r2), "r"(r3), "r"(r4), "r"(r5));
 	return arg1;
 }
 
