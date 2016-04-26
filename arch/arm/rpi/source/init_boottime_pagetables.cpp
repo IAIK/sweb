e98eb50d5080e1d2682b567fc023460840789d04
Daniel Kales <daniel.kales@gmail.com>
2016-04-26 19:20:47 +0200

    fixed raspi2 offsets

--- arch/arm/rpi/source/init_boottime_pagetables.cpp
+++ arch/arm/rpi/source/init_boottime_pagetables.cpp
@@ -46,11 +46,11 @@ extern "C" void initialiseBootTimePaging()
   for (i = 0; i < 1024; ++i)
     mapBootTimePage(pde_start, 3072 + i, i);
   // map devices from 0x81000000 upwards
-  mapBootTimePage(pde_start,0x860,0x202);  // pl011
-  mapBootTimePage(pde_start,0x8C0,0x203);  // emmc
-  mapBootTimePage(pde_start,0x900,0x200);  // most devices (ic, timer, gpu, ...)
+  mapBootTimePage(pde_start,0x860,0x3F2);  // pl011
+  mapBootTimePage(pde_start,0x8C0,0x3F3);  // emmc
+  mapBootTimePage(pde_start,0x900,0x3F0);  // most devices (ic, timer, gpu, ...)
 
-  mapBootTimePage(pde_start,0x909,0x209);  // map for csud
+  mapBootTimePage(pde_start,0x909,0x3F9);  // map for csud
 
   for (i = 0; i < 8; ++i)
     mapBootTimePage(pde_start,0xb00 + i,0x5c0 +i);  // framebuffer TODO: this might be not necessary
