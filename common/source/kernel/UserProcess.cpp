afd8afdd8fa9ada9a900b2475b6fc298f0f81b62
Daniel Gruss <gruss@student.tugraz.at>
2016-09-02 11:20:14 +0100

    removed old comment

--- common/source/kernel/UserProcess.cpp
+++ common/source/kernel/UserProcess.cpp
@@ -27,10 +27,10 @@ UserProcess::UserProcess(ustl::string filename, FileSystemInfo *fs_info, Process
   }
 
   size_t page_for_stack = PageManager::instance()->allocPPN();
-  loader_->arch_memory_.mapPage(USER_BREAK / PAGE_SIZE - 1, page_for_stack, 1); // (1024 * 512 - 1) * 4 KiB is exactly 2GiB - 4KiB
+  loader_->arch_memory_.mapPage(USER_BREAK / PAGE_SIZE - 1, page_for_stack, 1);
 
   ArchThreads::createUserRegisters(user_registers_, loader_->getEntryFunction(),
-                                   (void*) (USER_BREAK - sizeof(pointer)), // 2GiB - 4 Byte
+                                   (void*) (USER_BREAK - sizeof(pointer)),
                                    getStackStartPointer());
 
   ArchThreads::setAddressSpace(this, loader_->arch_memory_);
