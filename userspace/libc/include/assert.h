b1518cf46f6f05e464ba2ed49ba12265d1ac0ae1
Felix Kirchengast <felix.kirchengast@student.tugraz.at>
2016-09-14 12:53:17 +0200

    Added userspace assert

---
+++ userspace/libc/include/assert.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define assert(X) do { if (!(X)) {\
+    printf("Assertion  failed: '" #X "', file %s, function %s, line %d\n", __FILE__, __FUNCTION__, __LINE__); \
+ exit(-1); } } while (0)
+
