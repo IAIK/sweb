cf181bb9fbaa6c0aa15de19777f724d018c0de4c
Daniel Gruss <daniel.gruss@iaik.tugraz.at>
2018-06-24 19:04:34 +0200

    Update UserProcess.cpp

--- common/source/kernel/UserProcess.cpp
+++ common/source/kernel/UserProcess.cpp
@@ -27,7 +27,7 @@ UserProcess::UserProcess(ustl::string filename, FileSystemInfo *fs_info, uint32 
 
   size_t page_for_stack = PageManager::instance()->allocPPN();
   bool vpn_mapped = loader_->arch_memory_.mapPage(USER_BREAK / PAGE_SIZE - 1, page_for_stack, 1);
-  assert(vpn_mapped && "Virtual page for stack was already mapped");
+  assert(vpn_mapped && "Virtual page for stack was already mapped - this should never happen");
 
   ArchThreads::createUserRegisters(user_registers_, loader_->getEntryFunction(),
                                    (void*) (USER_BREAK - sizeof(pointer)),
