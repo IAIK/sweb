20ad1f60781292d63bbc70e79b7cd5cbda504ee8
Michael Schwarz <michael.schwarz@iaik.tugraz.at>
2018-06-24 23:16:36 +0200

    ARM: support boot on multicore systems

--- arch/arm/common/source/boot.cpp
+++ arch/arm/common/source/boot.cpp
@@ -10,6 +10,7 @@ extern uint8 bss_start_address;
 extern uint8 bss_end_address;
 extern PageDirEntry kernel_page_directory[];
 extern uint8 boot_stack[];
+uint32 multicore_sync __attribute__ ((section ("multicore")));
 
 #define BOOT_OFFSET (BOARD_LOAD_BASE - 0x80000000)
 
@@ -17,6 +18,13 @@ extern "C" void __attribute__((naked)) entry()
 {
   asm("mov fp, #0\n"
       "mov sp, %[v]" : : [v]"r"(((uint8*)boot_stack) + BOOT_OFFSET + 0x4000)); // Set up the stack
+
+  // only cpu 0 is allowed to continue, stop all other cores here
+  if(__atomic_fetch_add((uint32*)(((uint8*)&multicore_sync) + BOOT_OFFSET), 1, __ATOMIC_SEQ_CST)) {
+    while(1) {
+      asm volatile("mcr p15, 0, r0, c7, c0, 4" : : : "r0"); // hint to set cpu core to sleep mode
+    }
+  }
 
   void (*memsetPTR)(void*,uint8,size_t) = (void(*)(void*,uint8,size_t))((uint8*)&memset + BOOT_OFFSET);
   memsetPTR((void*)(&bss_start_address - BOOT_OFFSET), 0, (uint32)&bss_end_address - (uint32)&bss_start_address);
@@ -33,6 +41,7 @@ extern "C" void __attribute__((naked)) entry()
       "orr r0, r0, #0x800000\n"     // set disable subpages bit
       "mcr p15, 0, r0, c1, c0, 0\n" // enable paging
      );
+
   void (*PagingModePTR)() = &PagingMode; // create a blx jump instead of a bl jump
   PagingModePTR();
   assert(false && "it should be impossible to get to this point");
@@ -45,9 +54,11 @@ extern "C" void __attribute__((naked)) PagingMode()
       "orr r0, r0, #0xdf\n"
       "msr cpsr, r0\n");
   asm("mov sp, %[v]\n" : : [v]"r"(((uint32*)boot_stack)+4096)); // Set up the stack
+
   void (*startupPTR)() = &startup; // create a blx jump instead of a bl jump
   startupPTR();
   assert(false && "you should never get to this point");
+
 }
 
 void mapBootTimePage(PageDirEntry *pde_start, uint32 pdi, uint32 ppn_1m)
