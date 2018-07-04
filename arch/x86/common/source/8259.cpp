1b51d19aaee464f2a41c5cf8ff06db0fe2a946fb
Florian Kargl <florian.kargl@student.tugraz.at>
2018-07-04 16:47:39 +0200

    Defines for PIC 8259 commands

--- arch/x86/common/source/8259.cpp
+++ arch/x86/common/source/8259.cpp
@@ -6,17 +6,23 @@ volatile size_t outstanding_EOIs = 0;
 
 uint32 cached_mask = 0xFFFF;
 
+#define PIC_ICW1_INIT   0x11
+#define PIC_ICW2_OFFSET 0x20
+#define PIC_ICW4_8086   0x01
+
+#define PIC_EOI 0x20
+
 void initialise8259s()
 {
-  outportb(PIC_1_CONTROL_PORT, 0x11); /* ICW1 */
-  outportb(PIC_1_DATA_PORT, 0x20); /* ICW2: route IRQs 0...7 to INTs 20h...27h */
+  outportb(PIC_1_CONTROL_PORT, PIC_ICW1_INIT); /* ICW1 */
+  outportb(PIC_1_DATA_PORT, PIC_ICW2_OFFSET); /* ICW2: route IRQs 0...7 to INTs 20h...27h */
   outportb(PIC_1_DATA_PORT, 0x04); /* ICW3 */
-  outportb(PIC_1_DATA_PORT, 0x01); /* ICW4 */
+  outportb(PIC_1_DATA_PORT, PIC_ICW4_8086); /* PIC_ICW4 */
 
-  outportb(PIC_2_CONTROL_PORT, 0x11);
-  outportb(PIC_2_DATA_PORT, 0x28); /* ...IRQs 8...15 to INTs 28h...2Fh */
+  outportb(PIC_2_CONTROL_PORT, PIC_ICW1_INIT);
+  outportb(PIC_2_DATA_PORT, PIC_ICW2_OFFSET + 0x8); /* ...IRQs 8...15 to INTs 28h...2Fh */
   outportb(PIC_2_DATA_PORT, 0x02);
-  outportb(PIC_2_DATA_PORT, 0x01);
+  outportb(PIC_2_DATA_PORT, PIC_ICW4_8086);
 
   uint16 i;
   for (i=0;i<16;++i)
@@ -61,7 +67,7 @@ void sendEOI(uint16 number)
   --outstanding_EOIs;
   debug(A_INTERRUPTS, "sendEOI, outstanding: %zu\n", outstanding_EOIs);
   if (number > 7)
-    outportb(PIC_2_CONTROL_PORT,0x20);
+    outportb(PIC_2_CONTROL_PORT, PIC_EOI);
 
-  outportb(PIC_1_CONTROL_PORT,0x20);
+  outportb(PIC_1_CONTROL_PORT, PIC_EOI);
 }
