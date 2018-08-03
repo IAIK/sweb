282a921f361dca88d01e78aaf66911b527d4015f
Florian Kargl <florian.kargl@student.tugraz.at>
2018-08-03 23:17:11 +0200

    Print kernel panics/assertions to frame buffer

--- arch/x86/common/source/assert.cpp
+++ arch/x86/common/source/assert.cpp
@@ -69,6 +69,7 @@ void sweb_assert(const char *condition, uint32 line, const char* file)
   ArchInterrupts::disableInterrupts();
   system_state = KPANIC;
   kprintfd("KERNEL PANIC: Assertion %s failed in File %s on Line %d\n",condition, file, line);
+  kprintf("KERNEL PANIC: Assertion %s failed in File %s on Line %d\n",condition, file, line);
   if (currentThread != 0)
     currentThread->printBacktrace(false);
   while(1);
