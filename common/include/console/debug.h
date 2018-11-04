d87ea91abb47203ca5d43b2e0474ccde2e21f04a
Sandro Letter <sandro.letter@student.tugraz.at>
2018-11-04 21:42:05 +0100

    Update debug.h
    Update debug.h

--- common/include/console/debug.h
+++ common/include/console/debug.h
@@ -51,7 +51,7 @@ const size_t MAIN               = Ansi_Red     | OUTPUT_ENABLED;
 const size_t THREAD             = Ansi_Magenta | OUTPUT_ENABLED;
 const size_t USERPROCESS        = Ansi_Cyan    | OUTPUT_ENABLED;
 const size_t PROCESS_REG        = Ansi_Yellow  | OUTPUT_ENABLED;
-const size_t BACKTRACE          = Ansi_Red     | OUTPUT_ENABLED;
+const size_t BACKTRACE          = Ansi_Cyan    | OUTPUT_ENABLED;
 const size_t USERTRACE          = Ansi_Red     | OUTPUT_ENABLED;
 
 //group memory management
