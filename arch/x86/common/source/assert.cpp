186db50a11b99ae46aadf8bd4500ec97a9919faa
Florian Kargl <florian.kargl@student.tugraz.at>
2019-01-30 20:58:50 +0100

    Delay assertion print to prevent clobbering

--- arch/x86/common/source/assert.cpp
+++ arch/x86/common/source/assert.cpp
@@ -68,12 +68,14 @@ void sweb_assert(const char *condition, uint32 line, const char* file)
 {
   ArchInterrupts::disableInterrupts();
   system_state = KPANIC;
-  kprintfd("KERNEL PANIC: Assertion %s failed in File %s on Line %d, cpu %zd\n", condition, file, line, ArchMulticore::getCpuID());
-  kprintf("KERNEL PANIC: Assertion %s failed in File %s on Line %d, cpu %zd\n", condition, file, line, ArchMulticore::getCpuID());
-  if (currentThread != 0)
-    currentThread->printBacktrace(false);
   debug_print_to_fb = 0;
   ArchMulticore::stopAllCpus();
+  volatile size_t wait = 0x10000000;
+  while(--wait); // Dumb wait to allow other CPUs to finish printing debug output
+  if (currentThread != 0)
+          currentThread->printBacktrace(false);
+  kprintfd("KERNEL PANIC: Assertion %s failed in File %s on Line %d, cpu %zd\n", condition, file, line, ArchMulticore::getCpuID());
+  kprintf("KERNEL PANIC: Assertion %s failed in File %s on Line %d, cpu %zd\n", condition, file, line, ArchMulticore::getCpuID());
   while(1);
   unreachable();
 }
