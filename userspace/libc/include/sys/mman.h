61de087556b4d00a9d9ba84f34ac9e7cebfb46c6
Oskar Walcher <owalcher@student.tugraz.at>
2020-06-18 14:30:53 +0200

    change MAP_PRIVATE to 0x20000000 to be checkable

--- userspace/libc/include/sys/mman.h
+++ userspace/libc/include/sys/mman.h
@@ -12,7 +12,7 @@ extern "C" {
 #define PROT_WRITE    0x00000002  // ..0010
 #define PROT_EXEC     0x00000004  // ..0100
 
-#define MAP_PRIVATE   0x00000000  // 00..00
+#define MAP_PRIVATE   0x20000000  // 0010..
 #define MAP_SHARED    0x40000000  // 0100..
 #define MAP_ANONYMOUS 0x80000000  // 1000..
 
