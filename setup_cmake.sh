0c1b9e56af1cef8278f43237693dce3e4649ffd4
Florian Kargl <florian.kargl@student.tugraz.at>
2021-02-28 23:42:32 +0100

    Add cmake build directory setup script

---
+++ setup_cmake.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+# Use BUILD_DIR env var as path if provided, otherwise use default
+DEFAULT_DIR="/tmp/sweb"
+BUILD_DIR="${BUILD_DIR:-${DEFAULT_DIR}}"
+
+# Create the build directory and initialize cmake pointing to this source folder
+cmake -B"${BUILD_DIR}" -H.
