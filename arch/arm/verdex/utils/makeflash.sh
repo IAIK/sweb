51f430403c526327e2c2adfe75e78a1144b33b89
Woazboat <Woazboat@users.noreply.github.com>
2021-03-01 23:22:31 +0100

    Resize disk image to next power of two + error check for arm verdex build (#247)
    * Resize disk image to next power of two for use as sd card
    
    Required by newer versions of qemu: https://www.mail-archive.com/qemu-devel@nongnu.org/msg723028.html
    
    * Abort arm verdex flash image creation when kernel is too large

--- arch/arm/verdex/utils/makeflash.sh
+++ arch/arm/verdex/utils/makeflash.sh
@@ -1,10 +1,20 @@
-#!/bin/bash
-mkimage -A arm -O linux -T kernel -a 0xA0000000 -e 0xA0001000 -C none -n "SWEB ARM Gumstix Verdex kernel.x" -d kernel.x kernel.img
-if [[ "$?" != "0" ]]; then
-  echo "Consider installing u-boot-tools"
-  exit -1
-fi
-dd of=flash.img bs=128k count=256 if=/dev/zero
-dd of=flash.img bs=128k conv=notrunc if=$1
-dd of=flash.img bs=128k conv=notrunc seek=248 if=kernel.img
+#!/usr/bin/env sh
 
+FLASH_IMAGE=flash.img
+KERNEL_BIN=kernel.x
+KERNEL_IMAGE=kernel.img
+
+mkimage -A arm -O linux -T kernel -a 0xA0000000 -e 0xA0001000 -C none -n "SWEB ARM Gumstix Verdex kernel.x" -d ${KERNEL_BIN} ${KERNEL_IMAGE}
+if [ "$?" != "0" ]; then
+  echo "Consider installing u-boot-tools"
+  exit 1
+fi
+
+if [ -n "$(find "${KERNEL_IMAGE}" -prune -size +1000000c)" ]; then
+    echo "ERROR: Kernel image is too large for arm verdex flash (max 1Mib)"
+    exit 1
+fi
+
+dd of=${FLASH_IMAGE} bs=128k count=256 if=/dev/zero
+dd of=${FLASH_IMAGE} bs=128k conv=notrunc if=$1
+dd of=${FLASH_IMAGE} bs=128k conv=notrunc seek=248 if=${KERNEL_IMAGE}
