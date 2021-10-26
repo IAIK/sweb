572516adb447be4fdab440b5f262fb8d7630e6a9
Florian Kargl <florian.kargl@student.tugraz.at>
2021-10-26 23:20:30 +0200

    Reenable shell as default init process

--- common/include/kernel/user_progs.h
+++ common/include/kernel/user_progs.h
@@ -3,8 +3,6 @@
 // DO NOT CHANGE THE NAME OR THE TYPE OF THE user_progs VARIABLE!
 char const *user_progs[] = {
 // for reasons of automated testing
-        "/usr/helloworldloop.sweb",
-        "/usr/helloworldloop.sweb",
-        //"/usr/shell.sweb",
+        "/usr/shell.sweb",
         0
 };
