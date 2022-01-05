46d586e439087251d1a6b5167dd9ad0fb6b8d68f
Florian Kargl <f.kargl@posteo.de>
2022-01-05 19:46:02 +0100

    Add __cxa_guard functions for thread safe static initialization

--- common/source/util/libc++.cpp
+++ common/source/util/libc++.cpp
@@ -1,7 +1,77 @@
-
+#include "uatomic.h"
 
 extern "C" int __cxa_thread_atexit(__attribute__((unused)) void (*func)(), __attribute__((unused)) void *obj, __attribute__((unused)) void *dso_symbol)
 {
-        // Required for thread_local keyword (used for cpu local storage). Should technically call destructors of thread local objects, but we don't care about that (for now)
+        // Required for thread_local keyword (used for cpu local storage). Should technically call destructors of thread local objects (respectively cpu local in this case), but we don't care about that since we don't want to destroy cpu local storage
         return 0;
+}
+
+
+// https://android.googlesource.com/platform/bionic/+/c9a659c/libc/bionic/__cxa_guard.cpp
+
+#if defined(__arm__)
+// The ARM C++ ABI mandates that guard variables are 32-bit aligned, 32-bit
+// values. The LSB is tested by the compiler-generated code before calling
+// __cxa_guard_acquire.
+union _guard_t {
+    ustl::atomic<int> state;
+    int32_t aligner;
+};
+static_assert(sizeof(_guard_t) == sizeof(int32));
+#else
+// The Itanium/x86 C++ ABI (used by all other architectures) mandates that
+// guard variables are 64-bit aligned, 64-bit values. The LSB is tested by
+// the compiler-generated code before calling __cxa_guard_acquire.
+union _guard_t {
+    ustl::atomic<int> state;
+    int64_t aligner;
+};
+static_assert(sizeof(_guard_t) == sizeof(int64));
+#endif
+
+
+// Set construction state values according to reference documentation.
+// 0 is the initialization value.
+// Arm requires ((*gv & 1) == 1) after __cxa_guard_release, ((*gv & 3) == 0) after __cxa_guard_abort.
+// X86 requires first byte not modified by __cxa_guard_acquire, first byte is non-zero after
+// __cxa_guard_release.
+#define CONSTRUCTION_NOT_YET_STARTED                0
+#define CONSTRUCTION_COMPLETE                       1
+#define CONSTRUCTION_UNDERWAY                   0x100
+
+extern "C" int __cxa_guard_acquire(_guard_t* gv) {
+  int old_value = gv->state.load(ustl::memory_order_relaxed);
+  while (true) {
+    if (old_value == CONSTRUCTION_COMPLETE)
+    {
+      // A load_acquire operation is need before exiting with COMPLETE state, as we have to ensure
+      // that all the stores performed by the construction function are observable on this CPU
+      // after we exit.
+      ustl::atomic_thread_fence(ustl::memory_order_acquire);
+      return 0;
+    }
+    else if (old_value == CONSTRUCTION_NOT_YET_STARTED)
+    {
+      // Spinlock via compare exchange
+      if (!gv->state.compare_exchange_weak(old_value,
+                                           CONSTRUCTION_UNDERWAY,
+                                           ustl::memory_order_release,
+                                           ustl::memory_order_acquire)) {
+        continue;
+      }
+      // The acquire fence may not be needed. But as described in section 3.3.2 of
+      // the Itanium C++ ABI specification, it probably has to behave like the
+      // acquisition of a mutex, which needs an acquire fence.
+      ustl::atomic_thread_fence(ustl::memory_order_acquire);
+      return 1;
+    }
+  }
+}
+
+extern "C" void __cxa_guard_release(_guard_t* gv) {
+  gv->state.store(CONSTRUCTION_COMPLETE, ustl::memory_order_release);
+}
+
+extern "C" void __cxa_guard_abort(_guard_t* gv) {
+  gv->state.store(CONSTRUCTION_NOT_YET_STARTED, ustl::memory_order_release);
 }
