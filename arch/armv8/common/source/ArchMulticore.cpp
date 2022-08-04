6b8604f40edcfd71683f240cd9af990921103541
Florian Kargl <f.kargl@posteo.de>
2022-08-04 17:19:29 +0200

    Properly read armv8 cpu id

--- arch/armv8/common/source/ArchMulticore.cpp
+++ arch/armv8/common/source/ArchMulticore.cpp
@@ -1,13 +1,19 @@
 #include "ArchMulticore.h"
 #include "EASTL/atomic.h"
+#include "debug.h"
 
 eastl::atomic<size_t> running_cpus;
 Mutex ArchMulticore::cpu_list_lock_("CPU list lock");
 eastl::vector<CpuInfo*> ArchMulticore::cpu_list_;
+cpu_local size_t cpu_id;
+cpu_local CpuInfo cpu_info;
 
 size_t ArchMulticore::getCpuID()
 {
-    return 0;
+    uint64_t mpidr_el1 = 0;
+    asm("MRS %[mpidr_el1], MPIDR_EL1\n"
+        :[mpidr_el1]"=g"(mpidr_el1));
+    return mpidr_el1 & 0xFF;
 }
 
 size_t ArchMulticore::numRunningCPUs()
@@ -32,7 +38,20 @@ void ArchMulticore::initialize()
     assert(running_cpus == 0);
     running_cpus = 1;
     // CPULocalStorage::setCLS(CPULocalStorage::allocCLS());
-    // ArchMulticore::initCPULocalData(true);
+    ArchMulticore::initCPULocalData(true);
+}
+
+void ArchMulticore::initCPULocalData([[maybe_unused]]bool boot_cpu)
+{
+
+    // The constructor of objects declared as cpu_local will be called automatically the first time the cpu_local object is used. Other cpu_local objects _may or may not_ also be initialized at the same time.
+    new (&cpu_info) CpuInfo();
+    debug(A_MULTICORE, "Initializing CPU local objects for CPU %zu\n", cpu_info.getCpuID());
+
+    // idle_thread = new IdleThread();
+    // debug(A_MULTICORE, "CPU %zu: %s initialized\n", getCpuID(), idle_thread->getName());
+    // idle_thread->pinned_to_cpu = getCpuID();
+    // Scheduler::instance()->addNewThread(idle_thread);
 }
 
 void ArchMulticore::startOtherCPUs()
@@ -40,11 +59,22 @@ void ArchMulticore::startOtherCPUs()
 
 }
 
+CpuInfo::CpuInfo() :
+    cpu_id_(&cpu_id)
+{
+    setCpuID(ArchMulticore::getCpuID());
+    debug(A_MULTICORE, "Initializing CpuInfo %zx\n", getCpuID());
+    MutexLock l(ArchMulticore::cpu_list_lock_);
+    ArchMulticore::cpu_list_.push_back(this);
+    debug(A_MULTICORE, "Added CpuInfo %zx to cpu list\n", getCpuID());
+}
+
 size_t CpuInfo::getCpuID()
 {
-    return 0;
+    return *cpu_id_;
 }
 
 void CpuInfo::setCpuID([[maybe_unused]]size_t id)
 {
+    *cpu_id_ = id;
 }
