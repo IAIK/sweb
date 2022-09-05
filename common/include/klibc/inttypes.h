962b5f9b27195840326f35db0ec379879895e9dc
Florian Kargl <f.kargl@posteo.de>
2022-09-05 18:12:09 +0200

    Add barebones inttypes.h libc header

--- common/include/klibc/inttypes.h
+++ common/include/klibc/inttypes.h
@@ -1,1 +1,21 @@
 #pragma once
+
+#if __LONG_WIDTH__ == 32
+  #define __PRI_32_PREFIX "l"
+  #define __PRI_64_PREFIX "ll"
+#elif __LONG_WIDTH__ == 64
+  #define __PRI_32_PREFIX ""
+  #define __PRI_64_PREFIX "l"
+#else
+  #error "invalid sizeof long"
+#endif
+
+#define PRIx32 __PRI_32_PREFIX "x"
+#define PRId32 __PRI_32_PREFIX "d"
+#define PRIi32 __PRI_32_PREFIX "i"
+#define PRIu32 __PRI_32_PREFIX "u"
+
+#define PRIx64 __PRI_64_PREFIX "x"
+#define PRId64 __PRI_64_PREFIX "d"
+#define PRIi64 __PRI_64_PREFIX "i"
+#define PRIu64 __PRI_64_PREFIX "u"
