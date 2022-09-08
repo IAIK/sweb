48433ac7510ed9df9a277d6f37b36122cf52709c
Florian Kargl <f.kargl@posteo.de>
2022-09-08 21:53:01 +0200

    Fix 32 bit elf parsing for dwarf debug data

--- utils/add-debug/elf/elf.cc
+++ utils/add-debug/elf/elf.cc
@@ -19,7 +19,8 @@ void canon_hdr(Hdr<Elf64, byte_order::native> *out, const void *data,
                 case elfdata::msb:
                         out->from(*(Hdr<Elf32, byte_order::msb>*)data);
                         break;
-                } // fallthrough
+                }
+                break;
         case elfclass::_64:
                 switch (ei_data) {
                 case elfdata::lsb:
@@ -28,7 +29,8 @@ void canon_hdr(Hdr<Elf64, byte_order::native> *out, const void *data,
                 case elfdata::msb:
                         out->from(*(Hdr<Elf64, byte_order::msb>*)data);
                         return;
-                } // fallthrough
+                }
+                break;
         }
 }
 
