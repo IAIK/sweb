7a3f15b6aaced270997601be253c0ed8eea3ba03
Florian Kargl <f.kargl@posteo.de>
2022-10-30 09:27:18 +0100

    Pass call location to MutexLock

--- common/include/kernel/MutexLock.h
+++ common/include/kernel/MutexLock.h
@@ -1,22 +1,21 @@
 #pragma once
 
 #include "types.h"
+#include "backtrace.h"
 
 class Mutex;
 
 class MutexLock
 {
   public:
-    MutexLock(Mutex &m);
-    MutexLock(Mutex &m, bool b);
+    MutexLock(Mutex& m, bool b = true, pointer called_by = getCalledBefore(0));
     ~MutexLock();
 
-    MutexLock(MutexLock const&) = delete;
-    MutexLock &operator=(MutexLock const&) = delete;
+    MutexLock(const MutexLock&) = delete;
+    MutexLock& operator=(const MutexLock&) = delete;
 
   private:
 
-    Mutex &mutex_;
+    Mutex& mutex_;
     bool use_mutex_;
 };
-
--- common/source/kernel/MutexLock.cpp
+++ common/source/kernel/MutexLock.cpp
@@ -2,25 +2,20 @@
 #include "MutexLock.h"
 #include "backtrace.h"
 
-MutexLock::MutexLock(Mutex &m) :
-  mutex_(m), use_mutex_(true)
+MutexLock::MutexLock(Mutex &m, bool b, pointer called_by) :
+    mutex_(m), use_mutex_(b)
 {
-  mutex_.acquire(getCalledBefore(1));
-}
-
-MutexLock::MutexLock(Mutex &m, bool b) :
-  mutex_(m), use_mutex_(b)
-{
-  if (likely(use_mutex_))
-  {
-    mutex_.acquire(getCalledBefore(1));
-  }
+    if (likely(use_mutex_))
+    {
+        mutex_.acquire(called_by);
+    }
 }
 
 MutexLock::~MutexLock()
 {
-  if (likely(use_mutex_))
-  {
-    mutex_.release(getCalledBefore(1));
-  }
+    if (likely(use_mutex_))
+    {
+        mutex_.release(getCalledBefore(1));
+        use_mutex_ = false;
+    }
 }
