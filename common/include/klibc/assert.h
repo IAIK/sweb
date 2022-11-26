1e65244ad5df0ce98224b4abd685db2cba35a5bd
Florian Kargl <f.kargl@posteo.de>
2022-11-26 13:09:51 +0100

    Remove duplicate assert.h header file

--- common/include/klibc/assert.h
+++ common/include/klibc/assert.h
@@ -2,22 +2,23 @@
 
 #include "stdint.h"
 
-
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 
-
-#define assert(cond) do { (cond) ? (void)0 : sweb_assert( #cond ,__LINE__,__FILE__); } while (0)
+#ifndef assert
+#define assert(cond) do { (cond) ? (void)0 : sweb_assert( #cond ,__LINE__,__FILE__, __PRETTY_FUNCTION__); } while (0)
+#endif
 
 /**
  * called when assertion is used and true
  * @param condition assertion that has to be checked
  * @param line the function which to jump to
  * @param file where the assertion is called
+ * @param function where the assertion is called
  */
-    __attribute__((noreturn)) void sweb_assert(const char *condition, uint32_t line, const char* file);
+    __attribute__((noreturn)) void sweb_assert(const char *condition, uint32_t line, const char* file, const char* function);
 
 
 #ifdef __cplusplus
