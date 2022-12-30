344d475e7f6e289e08a2814a8149a202611553a9
Florian Kargl <f.kargl@posteo.de>
2022-12-30 21:08:33 +0100

    Format shell.cpp

--- userspace/tests/shell.cpp
+++ userspace/tests/shell.cpp
@@ -1,16 +1,16 @@
-#include "unistd.h"
-#include "stdio.h"
-#include "string.h"
-#include "stdlib.h"
-#include "nonstd.h"
 #include "dirent.h"
 #include "fcntl.h"
-#include "assert.h"
+#include "nonstd.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
 #include "sys/syscall.h"
+#include "unistd.h"
+#include "assert.h"
 
 #define FORK_ENABLED 0
-#define EXECUTABLE_PREFIX       "/usr/"
-#define EXECUTABLE_PREFIX_LEN   5
+#define EXECUTABLE_PREFIX "/usr/"
+#define EXECUTABLE_PREFIX_LEN 5
 #define BUFFER_SIZE 256
 
 int running = 1;
@@ -26,15 +26,17 @@ char last_input[BUFFER_SIZE];
 char dirent_buffer[4096];
 ssize_t ndents = 0;
 
-typedef struct {
+struct autocomplete_result_t
+{
     const char* command;
     size_t str_len;
     size_t num_matches;
-} autocomplete_result_t;
+};
 
 const char* builtin_commands[] = {"ls", "exit", "help"};
 
-const char* d_type_str[] = { // see constants in dirent.h
+const char* d_type_str[] = {
+    // see constants in dirent.h
     "F", // File
     "D", // Directory
     "L", // Link
@@ -73,9 +75,8 @@ int ls(const char* path)
     for (size_t dpos = 0; dpos < static_cast<size_t>(ndents);)
     {
         dirent* dent = (dirent*)(dirent_buffer + dpos);
-        printf("[%s] %s\n", dent->d_type <= 4 ? d_type_str[dent->d_type] :
-                                                "?",
-                            dent->d_name);
+        printf("[%s] %s\n", dent->d_type <= 4 ? d_type_str[dent->d_type] : "?",
+               dent->d_name);
 
         dpos += dent->d_offs_next;
         if (dent->d_offs_next == 0)
@@ -87,269 +88,273 @@ int ls(const char* path)
 
 void handle_command(char* buffer, int buffer_size)
 {
-  int c = 0;
-  int argsCount = -1;
-  int lastIndex = 0;
-  int pid;
+    int c = 0;
+    int argsCount = -1;
+    int lastIndex = 0;
+    int pid;
 
-  for (c = 0; c < buffer_size; c++) //Do check for null termination at end of for loop to have one last run with \0
-  {
-    if (argsCount >= 10)
+    for (c = 0; c < buffer_size; c++) // Do check for null termination at end of for loop
+                                      // to have one last run with \0
     {
-      argsCount = 10;
-      printf("Argument count is limited to 10 (no dynamic memory allocation) - all other arguments will be ignored\n");
-      break;
+        if (argsCount >= 10)
+        {
+            argsCount = 10;
+            printf("Argument count is limited to 10 (no dynamic memory allocation) - all "
+                   "other arguments will be ignored\n");
+            break;
+        }
+        if (buffer[c] == '\r' || buffer[c] == '\n' || buffer[c] == ' ' ||
+            buffer[c] == '\0')
+        {
+            if (argsCount == -1)
+            {
+                memcpy(command, buffer + lastIndex, c - lastIndex);
+                command[c - lastIndex] = 0;
+            }
+            else
+            {
+                memcpy(args[argsCount], buffer + lastIndex, c - lastIndex);
+                args[argsCount][c - lastIndex] = 0;
+            }
+            argsCount++;
+            lastIndex = c + 1;
+        }
+
+        if (buffer[c] == '\0')
+        {
+            break;
+        }
     }
-    if (buffer[c] == '\r' || buffer[c] == '\n' || buffer[c] == ' ' || buffer[c] == '\0')
+
+    if (strcmp(command, "ls") == 0)
     {
-      if (argsCount == -1)
-      {
-        memcpy(command, buffer + lastIndex, c - lastIndex);
-        command[c - lastIndex] = 0;
-      }
-      else
-      {
-        memcpy(args[argsCount], buffer + lastIndex, c - lastIndex);
-        args[argsCount][c - lastIndex] = 0;
-      }
-      argsCount++;
-      lastIndex = c + 1;
+        ls((argsCount > 0 ? args[0] : "."));
     }
-
-    if (buffer[c] == '\0')
+    else if (strcmp(command, "help") == 0)
     {
-      break;
+        printf("Command Help:\n"
+               "help                  yes, here we are\n"
+               "exit [exit_code]      is really the only command that does something "
+               "right now\n"
+               "ls [path]             pseudo ls\n\n");
     }
-  }
-
-
-  if (strcmp(command, "ls") == 0)
-  {
-    ls((argsCount > 0 ? args[0] : "."));
-  }
-  else if (strcmp(command, "help") == 0)
-  {
-    printf(
-        "Command Help:\n"
-        "help                  yes, here we are\n"
-        "exit [exit_code]      is really the only command that does something right now\n"
-        "ls [path]             pseudo ls\n\n");
-  }
-  else if (strcmp(command, "exit") == 0)
-  {
-    c = 4;
-    while (buffer[c] == ' ')
-      c++;
-    exit_code = atoi(&buffer[c]);
-    printf("Exiting Shell with exit_code %d\n", exit_code);
-    running = 0;
-  }
-  else if (FORK_ENABLED)
-  {
-    pid = fork();
-    printf("NewPid: %d\n", pid);
-
-    if (pid == 0)
+    else if (strcmp(command, "exit") == 0)
     {
-      //child process, replace with new image
-      execv(command, 0);
-      printf("Command not understood\n");
-      exit(1);
+        c = 4;
+        while (buffer[c] == ' ')
+            c++;
+        exit_code = atoi(&buffer[c]);
+        printf("Exiting Shell with exit_code %d\n", exit_code);
+        running = 0;
+    }
+    else if (FORK_ENABLED)
+    {
+        pid = fork();
+        printf("NewPid: %d\n", pid);
+
+        if (pid == 0)
+        {
+            // child process, replace with new image
+            execv(command, 0);
+            printf("Command not understood\n");
+            exit(1);
+        }
+        else
+        {
+            // wait for child process
+            printf("Join on child process\n");
+        }
     }
     else
     {
-      //wait for child process
-      printf("Join on child process\n");
-    }
+        if (command[0] != '/')
+        {
+            memcpy(executable, EXECUTABLE_PREFIX, EXECUTABLE_PREFIX_LEN);
+            memcpy(executable + EXECUTABLE_PREFIX_LEN, command, strlen(command) + 1);
+        }
+        else
+        {
+            memcpy(executable, command, strlen(command) + 1);
+        }
 
-  }
-  else
-  {
-    if (command[0] != '/')
-    {
-      memcpy(executable, EXECUTABLE_PREFIX, EXECUTABLE_PREFIX_LEN);
-      memcpy(executable + EXECUTABLE_PREFIX_LEN, command, strlen(command) + 1);
+        pid = createprocess(executable, 1);
+        if (pid == -1)
+        {
+            printf("Command not understood\n");
+        }
     }
-    else
-    {
-      memcpy(executable, command, strlen(command) + 1);
-    }
-
-    pid = createprocess(executable, 1);
-    if (pid == -1)
-    {
-      printf("Command not understood\n");
-    }
-  }
 }
 
 autocomplete_result_t autocomplete(const char* user_input, const char* path)
 {
-  autocomplete_result_t res;
-  memset(&res, 0, sizeof(res));
+    autocomplete_result_t res;
+    memset(&res, 0, sizeof(res));
 
-  ssize_t ndents = getdentsBuffer(path, dirent_buffer, sizeof(dirent_buffer));
+    ssize_t ndents = getdentsBuffer(path, dirent_buffer, sizeof(dirent_buffer));
 
-  for (size_t dpos = 0; dpos < static_cast<size_t>(ndents);)
-  {
-      dirent* dent = (dirent*)(dirent_buffer + dpos);
+    for (size_t dpos = 0; dpos < static_cast<size_t>(ndents);)
+    {
+        dirent* dent = (dirent*)(dirent_buffer + dpos);
 
-      if (strstr(dent->d_name, user_input) == dent->d_name)
-      {
-          ++res.num_matches;
-          if (res.num_matches > 1)
-              printf("\n%s", dent->d_name);
-          if (!res.command)
-          {
-              res.command = dent->d_name;
-              res.str_len = strlen(res.command);
-          }
-          else
-          {
-              size_t i = 0;
-              while (i < res.str_len && res.command[i] && dent->d_name[i] && res.command[i] == dent->d_name[i])
-                  ++i;
-              res.str_len = i;
-          }
-      }
+        if (strstr(dent->d_name, user_input) == dent->d_name)
+        {
+            ++res.num_matches;
+            if (res.num_matches > 1)
+                printf("\n%s", dent->d_name);
+            if (!res.command)
+            {
+                res.command = dent->d_name;
+                res.str_len = strlen(res.command);
+            }
+            else
+            {
+                size_t i = 0;
+                while (i < res.str_len && res.command[i] && dent->d_name[i] &&
+                       res.command[i] == dent->d_name[i])
+                    ++i;
+                res.str_len = i;
+            }
+        }
 
-      dpos += dent->d_offs_next;
-      if (dent->d_offs_next == 0)
-          break;
-  }
+        dpos += dent->d_offs_next;
+        if (dent->d_offs_next == 0)
+            break;
+    }
 
-  for (size_t i = 0; i < sizeof(builtin_commands)/sizeof(builtin_commands[0]); ++i)
-  {
-      if (strstr(builtin_commands[i], user_input) == builtin_commands[i])
-      {
-          ++res.num_matches;
-          if (res.num_matches > 1)
-              printf("\n%s", builtin_commands[i]);
-          if (!res.command)
-          {
-              res.command = builtin_commands[i];
-              res.str_len = strlen(res.command);
-          }
-          else
-          {
-              size_t j = 0;
-              while (j < res.str_len && res.command[j] && builtin_commands[i][j] && res.command[j] == builtin_commands[i][j])
-                  ++j;
-              res.str_len = j;
-          }
-      }
-  }
+    for (size_t i = 0; i < sizeof(builtin_commands) / sizeof(builtin_commands[0]); ++i)
+    {
+        if (strstr(builtin_commands[i], user_input) == builtin_commands[i])
+        {
+            ++res.num_matches;
+            if (res.num_matches > 1)
+                printf("\n%s", builtin_commands[i]);
+            if (!res.command)
+            {
+                res.command = builtin_commands[i];
+                res.str_len = strlen(res.command);
+            }
+            else
+            {
+                size_t j = 0;
+                while (j < res.str_len && res.command[j] && builtin_commands[i][j] &&
+                       res.command[j] == builtin_commands[i][j])
+                    ++j;
+                res.str_len = j;
+            }
+        }
+    }
 
-  if (res.num_matches > 1)
-      printf("\n%s", res.command);
+    if (res.num_matches > 1)
+        printf("\n%s", res.command);
 
-  return res;
+    return res;
 }
 
 int readCommand(char* buffer, size_t buffer_size)
 {
-  unsigned int chars_in_buffer = 0;
-  char cchar;
-  char up_pressed = 0;
+    unsigned int chars_in_buffer = 0;
+    char cchar;
+    char up_pressed = 0;
 
-  memset(buffer, 0, buffer_size);
+    memset(buffer, 0, buffer_size);
 
-  while((cchar = getchar()) != EOF)
-  {
-    if(cchar == '\r' || cchar == '\n' || (chars_in_buffer + 1) >= buffer_size)
+    while ((cchar = getchar()) != EOF)
     {
-      buffer[chars_in_buffer] = '\0';
-      break;
+        if (cchar == '\r' || cchar == '\n' || (chars_in_buffer + 1) >= buffer_size)
+        {
+            buffer[chars_in_buffer] = '\0';
+            break;
+        }
+        else if (cchar == '\t') // autocomplete
+        {
+            if (chars_in_buffer == 0)
+                continue;
+
+            autocomplete_result_t res = autocomplete(buffer, EXECUTABLE_PREFIX);
+            if (!res.command || !res.str_len)
+                continue; // no such file
+
+            if (res.num_matches > 1)
+                printf("\nSWEB: %s> %s", cwd, buffer);
+
+            const char* completion = res.command;
+            completion += chars_in_buffer;
+
+            int i;
+            for (i = 0;
+                 completion[i] && completion[i] != '\n' &&
+                 chars_in_buffer < res.str_len && chars_in_buffer < BUFFER_SIZE - 1;
+                 i++, chars_in_buffer++)
+            {
+                buffer[chars_in_buffer] = completion[i];
+            }
+
+            write(STDOUT_FILENO, completion, i);
+            continue;
+        }
+        else if ((unsigned char)cchar == 151) // up: take last input
+        {
+            if (chars_in_buffer != 0)
+                continue;
+
+            up_pressed = 1;
+
+            chars_in_buffer = strlen(last_input);
+            memcpy(buffer, last_input, buffer_size);
+            write(STDOUT_FILENO, buffer, chars_in_buffer);
+            continue;
+        }
+        else if ((unsigned char)cchar == 152) // down: drop input
+        {
+            if (chars_in_buffer == 0 || !up_pressed)
+                continue;
+
+            memset(buffer, '\b', chars_in_buffer);
+            buffer[chars_in_buffer] = 0;
+            printf("%s", buffer);
+            memset(buffer, 0, buffer_size);
+            chars_in_buffer = 0;
+            up_pressed = 0;
+            continue;
+        }
+        else
+        {
+            buffer[chars_in_buffer] = (char)cchar;
+        }
+
+        if (cchar == '\b')
+        {
+            if (chars_in_buffer > 0)
+            {
+                buffer[chars_in_buffer] = 0;
+                chars_in_buffer--;
+            }
+        }
+        else
+        {
+            chars_in_buffer++;
+        }
     }
-    else if(cchar == '\t') // autocomplete
-    {
-      if(chars_in_buffer == 0)
-        continue;
 
-      autocomplete_result_t res = autocomplete(buffer, EXECUTABLE_PREFIX);
-      if(!res.command || !res.str_len)
-        continue; // no such file
-
-      if (res.num_matches > 1)
-          printf("\nSWEB: %s> %s", cwd, buffer);
-
-      const char* completion = res.command;
-      completion += chars_in_buffer;
-
-      int i;
-      for(i = 0; completion[i] && completion[i] != '\n' && chars_in_buffer < res.str_len && chars_in_buffer < BUFFER_SIZE - 1; i++, chars_in_buffer++)
-      {
-          buffer[chars_in_buffer] = completion[i];
-      }
-
-      write(STDOUT_FILENO, completion, i);
-      continue;
-    }
-    else if((unsigned char)cchar == 151) // up: take last input
-    {
-      if(chars_in_buffer != 0)
-        continue;
-
-      up_pressed = 1;
-
-      chars_in_buffer = strlen(last_input);
-      memcpy(buffer, last_input, buffer_size);
-      write(STDOUT_FILENO, buffer, chars_in_buffer);
-      continue;
-    }
-    else if((unsigned char)cchar == 152) // down: drop input
-    {
-      if(chars_in_buffer == 0 || !up_pressed)
-        continue;
-
-      memset(buffer, '\b', chars_in_buffer);
-      buffer[chars_in_buffer] = 0;
-      printf("%s",buffer);
-      memset(buffer, 0, buffer_size);
-      chars_in_buffer = 0;
-      up_pressed = 0;
-      continue;
-    }
-    else
-    {
-      buffer[chars_in_buffer] = (char)cchar;
-    }
-
-    if (cchar == '\b')
-    {
-      if (chars_in_buffer > 0)
-      {
-        buffer[chars_in_buffer] = 0;
-        chars_in_buffer--;
-      }
-    }
-    else
-    {
-      chars_in_buffer++;
-    }
-  }
-
-  buffer[BUFFER_SIZE - 1] = 0;
-  memcpy(last_input, buffer, buffer_size);
-  return 0;
+    buffer[BUFFER_SIZE - 1] = 0;
+    memcpy(last_input, buffer, buffer_size);
+    return 0;
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-  cwd[0] = '/';
-  cwd[1] = '\0';
+    cwd[0] = '/';
+    cwd[1] = '\0';
 
-  printf("\n\%s\n", "SWEB-Pseudo-Shell starting...\n");
+    printf("\n\nSWEB-Pseudo-Shell starting...\n\n");
 
-  do
-  {
-    printf("\nSWEB: %s> ", cwd);
-    readCommand(buffer, BUFFER_SIZE);
-    handle_command(buffer, BUFFER_SIZE);
-    for (size_t a = 0; a < BUFFER_SIZE; a++)
-      buffer[a] = 0;
+    do
+    {
+        printf("\nSWEB: %s> ", cwd);
+        readCommand(buffer, BUFFER_SIZE);
+        handle_command(buffer, BUFFER_SIZE);
+        memset(&buffer, 0, sizeof(buffer));
+    } while (running);
 
-  } while (running);
-
-  return exit_code;
+    return exit_code;
 }
