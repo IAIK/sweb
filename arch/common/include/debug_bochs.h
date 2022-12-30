8f72f29d9f6e2f2438b3e459e57e37094a2aecde
Florian Kargl <f.kargl@posteo.de>
2022-12-30 13:59:17 +0100

    Use static emulator debugconsole io port instead of ad-hoc outportb in writeChar2Bochs

--- arch/common/include/debug_bochs.h
+++ arch/common/include/debug_bochs.h
@@ -5,12 +5,11 @@
  * writes a char to the bochs terminal
  *
  */
-void writeChar2Bochs( char char2Write );
+void writeChar2Bochs(char char2Write);
 
 /**
  * writes a string/line to the bochs terminal
  * max length is 250
  *
  */
-void writeLine2Bochs( const char *line2Write );
-
+void writeLine2Bochs(const char *line2Write);
