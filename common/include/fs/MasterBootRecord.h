cc4965c3a464858f6cb80e130f24e0db89c3db58
Florian Kargl <f.kargl@posteo.de>
2023-03-14 16:13:44 +0100

    Add partition type list comment to MBR

--- common/include/fs/MasterBootRecord.h
+++ common/include/fs/MasterBootRecord.h
@@ -41,6 +41,36 @@ struct MasterBootRecord
 
 static_assert(sizeof(MasterBootRecord) == 512);
 
+/*
+    Partition types (taken from fdisk)
+
+    00 Empty            27 Hidden NTFS Win  82 Linux swap / So  c1 DRDOS/sec (FAT-
+    01 FAT12            39 Plan 9           83 Linux            c4 DRDOS/sec (FAT-
+    02 XENIX root       3c PartitionMagic   84 OS/2 hidden or   c6 DRDOS/sec (FAT-
+    03 XENIX usr        40 Venix 80286      85 Linux extended   c7 Syrinx
+    04 FAT16 <32M       41 PPC PReP Boot    86 NTFS volume set  da Non-FS data
+    05 Extended         42 SFS              87 NTFS volume set  db CP/M / CTOS / .
+    06 FAT16            4d QNX4.x           88 Linux plaintext  de Dell Utility
+    07 HPFS/NTFS/exFAT  4e QNX4.x 2nd part  8e Linux LVM        df BootIt
+    08 AIX              4f QNX4.x 3rd part  93 Amoeba           e1 DOS access
+    09 AIX bootable     50 OnTrack DM       94 Amoeba BBT       e3 DOS R/O
+    0a OS/2 Boot Manag  51 OnTrack DM6 Aux  9f BSD/OS           e4 SpeedStor
+    0b W95 FAT32        52 CP/M             a0 IBM Thinkpad hi  ea Linux extended
+    0c W95 FAT32 (LBA)  53 OnTrack DM6 Aux  a5 FreeBSD          eb BeOS fs
+    0e W95 FAT16 (LBA)  54 OnTrackDM6       a6 OpenBSD          ee GPT
+    0f W95 Ext'd (LBA)  55 EZ-Drive         a7 NeXTSTEP         ef EFI (FAT-12/16/
+    10 OPUS             56 Golden Bow       a8 Darwin UFS       f0 Linux/PA-RISC b
+    11 Hidden FAT12     5c Priam Edisk      a9 NetBSD           f1 SpeedStor
+    12 Compaq diagnost  61 SpeedStor        ab Darwin boot      f4 SpeedStor
+    14 Hidden FAT16 <3  63 GNU HURD or Sys  af HFS / HFS+       f2 DOS secondary
+    16 Hidden FAT16     64 Novell Netware   b7 BSDI fs          f8 EBBR protective
+    17 Hidden HPFS/NTF  65 Novell Netware   b8 BSDI swap        fb VMware VMFS
+    18 AST SmartSleep   70 DiskSecure Mult  bb Boot Wizard hid  fc VMware VMKCORE
+    1b Hidden W95 FAT3  75 PC/IX            bc Acronis FAT32 L  fd Linux raid auto
+    1c Hidden W95 FAT3  80 Old Minix        be Solaris boot     fe LANstep
+    1e Hidden W95 FAT1  81 Minix / old Lin  bf Solaris          ff BBT
+*/
+
 enum PartitionType
 {
     EMPTY = 0x00,
